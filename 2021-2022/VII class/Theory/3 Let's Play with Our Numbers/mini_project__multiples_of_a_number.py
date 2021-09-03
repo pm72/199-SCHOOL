b6d0fccc446c8436ebe3d1d803e29dbade6ea3ce
paata <pm72github@yahoo.com>
2021-09-03 16:21:25 +0400

    pm72

---
+++ 2021-2022/VII class/Theory/3 Let's Play with Our Numbers/mini_project__multiples_of_a_number.py
@@ -0,0 +1,21 @@
+# range
+
+### up
+##print(range(5))
+##print(range(1, 10))
+##
+##r = range(5)
+##print(*r)
+##print(*range(1, 10))
+##
+##print(*range(10))
+##
+##print(*range(0, 10, 2))
+##print(*range(3, 101, 3))
+
+
+### down
+##print(*range(10, 1, -1))
+##print(*range(10, 0, -1))
+##print(*range(10, -1, -1))
+##print(*range(157, 22, -7))
