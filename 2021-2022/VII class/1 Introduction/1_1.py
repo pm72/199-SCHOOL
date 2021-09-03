c6b065c9f5362fdb0b5cbb50ad7e4344582299cb
paata <pm72github@yahoo.com>
2021-09-03 12:55:42 +0400

    Initial commit

---
+++ 2021-2022/VII class/1 Introduction/1_1.py
@@ -0,0 +1,18 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 3 + 6
+9
+>>> 
+>>> (235 + 542) / (564 + 123)
+1.1310043668122272
+>>> 
+>>> print("Hello there!")
+Hello there!
+>>> 
+>>> Print("Hello")
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    Print("Hello")
+NameError: name 'Print' is not defined
+>>> 
+>>> 
---
+++ 2021-2022/VII class/1 Introduction/introduction.py
@@ -0,0 +1,4 @@
+print("Hello There!")
+print("My name is Paata Mamporia.")
+print("I am 49 years old.")
+print("I am Teacher.")
