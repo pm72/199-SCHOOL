e8c86e9943ce288ab108b01ca0ed885271eddee2
paata <pm72github@yahoo.com>
2021-09-03 14:11:07 +0400

    pm72

---
+++ 2021-2022/VII class/Theory/2 Python Loves Numbers/complex_numbers.py
@@ -0,0 +1,67 @@
+num1 = 3
+num2 = 5.5
+num3 = 2 + 3j
+
+##print(type(num1))
+##print(type(num2))
+##print(type(num3))
+
+
+# =================
+##print(num3.real)
+##print(num3.imag)
+
+
+# =================
+##print(3+4j.imag)
+##print((3+4j).imag)
+
+
+
+# Type conversion between numbers
+##num1 = 3.0
+##print(num1)
+##print(int(num1))
+
+
+# ==================
+##num1 = 3.45
+##print(num1)
+##print(int(num1))
+##
+##num1 = 3.95
+##print(num1)
+##print(int(num1))
+
+
+# =====================
+##num1 = 3 + 4j
+##print(num1)
+##print(int(num1))
+
+
+# =====================
+##num1 = 3 + 4j
+##r = num1.real
+##print(r)
+##print(int(r))
+##
+##print(int(num1.real))
+
+
+# =======================
+##num1 = 3
+##print(num1)
+##print(float(num1))
+
+
+# ======================
+##num1 = 3
+##print(num1)
+##print(complex(num1))
+##
+##print()
+##
+##num1 = 3.54
+##print(num1)
+##print(complex(num1))
