aab126af818d48d04215cfd781a0bcbcdc865660
pm72 <46081859+pm72@users.noreply.github.com>
2021-09-15 09:25:23 +0400

    pm72

---
+++ 2021-2022/Theory/4 Drawing Cool Stuff with Turtle/ch_4_1__draw_turtle.py
@@ -0,0 +1,14 @@
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+##t.forward(100)
+##t.home()
+##t.backward(100)
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.left(90)
+t.forward(100)
