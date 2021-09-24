c2ac19fa8ec0fd87b4dfb465f90636e3e0b11e34
paata <pm72github@yahoo.com>
2021-09-24 14:13:29 +0400

    pm72

---
+++ 2021-2022/VIII კლასი/8_5/002 (09.24.21)/2_3.py
@@ -0,0 +1,7 @@
+print("Hello there!")
+print("My name is Paata mamporia")
+print('I am 49 years old...')
+print("I am a teacher.")
+
+
+input()
---
+++ 2021-2022/VIII კლასი/8_5/002 (09.24.21)/2_4.py
@@ -0,0 +1,6 @@
+x = 90
+y = 112
+
+print("x variable is", x, 'and y variavle is', y)
+
+input()
