7747be5a14a3c4e26c3073517ecd0fc9ba4cfc1b
paata <pm72github@yahoo.com>
2021-09-24 13:25:47 +0400

    pm72

---
+++ 2021-2022/VII კლასი/7_2/002 (09.24.21)/2_1.py
@@ -0,0 +1,17 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 3 + 5
+8
+>>> (289 + 312) / (11.58 + 56.97)
+8.767323121808898
+>>> print(54 + 98)
+152
+>>> print("Hello there")
+Hello there
+>>> print('Hello there')
+Hello there
+>>> x = 90
+>>> y = 67
+>>> print(x, y)
+90 67
+>>> 
---
+++ 2021-2022/VII კლასი/7_2/002 (09.24.21)/2_3.py
@@ -0,0 +1,4 @@
+print("Hello there!")
+print("My name is Paata mamporia")
+print('I am 49 years old...')
+print('I am a teacher.')
