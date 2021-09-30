9df1d5edef90e300212a93a02c92c46948b3a314
pm72 <46081859+pm72@users.noreply.github.com>
2021-09-30 19:23:35 +0400

    pm72

---
+++ 2021-2022/VIII კლასი/8_3/003 (09.30.21)/3_1.py
@@ -0,0 +1,28 @@
+# string     <class 'str'>
+x = 'Paata mamporia'
+print(x)
+print(id(x))
+print(id('Paata mamporia'))
+print(type(x))
+
+
+print()
+
+
+# int     <class 'int'
+x = 95
+print(x)
+print(id(x))
+print(id(95))
+print(type(x))
+
+
+print()
+
+
+# float     <class 'float'
+x = 9.5
+print(x)
+print(id(x))
+print(id(9.5))
+print(type(x))
