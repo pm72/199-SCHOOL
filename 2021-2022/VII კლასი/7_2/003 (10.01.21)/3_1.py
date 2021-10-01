df8dfda4aebfaa74212934dbee1dc655d81d9d6f
pm72 <46081859+pm72@users.noreply.github.com>
2021-10-02 00:13:22 +0400

    pm72

---
+++ 2021-2022/VII კლასი/7_2/003 (10.01.21)/3_1.py
@@ -0,0 +1,59 @@
+# string    <class 'str'>
+a = "Paata Mamporia 49 years old"
+print(a)
+print(id(a))
+print(id("Paata Mamporia 49 years old"))
+print(type(a))
+
+
+print()
+
+# int    <class 'int'>
+a = 49
+print(a)
+print(id(a))
+print(id(49))
+print(type(a))
+
+
+print()
+
+# float    <class 'float'>
+a = 4.9
+print(a)
+print(id(a))
+print(id(4.9))
+print(type(a))
+
+
+
+# ertstriqoniani komentari
+
+"""
+mravalstriqoniani
+komentari
+
+        x = 2.56
+        y = 3 * x - 1
+        d = x ** 2 + y ** 2
+"""
+
+
+'''
+mravalstriqoniani
+komentari
+
+        x = 2.56
+        y = 3 * x - 1
+        d = x ** 2 + y ** 2
+'''
+
+
+print("""
+mravalstriqoniani
+komentari
+
+        x = 2.56
+        y = 3 * x - 1
+        d = x ** 2 + y ** 2
+""")
