bb64c5453956955e616fc7ba5a02c9dafc18283b
pm72 <46081859+pm72@users.noreply.github.com>
2021-10-04 17:43:03 +0400

    pm72

---
+++ 2021-2022/VIII კლასი/8_6/003 (10.04.21)/3_1.py
@@ -0,0 +1,62 @@
+# string        <class 'str'>
+
+a = "Paata Mamporia is 49 years old"
+print(a)
+print(id(a))
+print(id("Paata Mamporia is 49 years old"))
+print(type(a))
+
+
+print()
+
+
+# int        <class 'int'>
+a = 49
+print(a)
+print(id(a))
+print(id(49))
+print(type(a))
+
+
+
+print()
+
+
+# float        <class 'float'>
+a = 4.9
+print(a)
+print(id(a))
+print(id(4.9))
+print(type(a))
+
+
+# ertstriqoniani komentari
+
+
+"""
+mravalstriqoniani
+komentari
+      a = 2.58
+      b = a * 5 - 2 * a
+      d = a ** 2 + b ** 2
+
+"""
+
+'''
+mravalstriqoniani
+komentari
+      a = 2.58
+      b = a * 5 - 2 * a
+      d = a ** 2 + b ** 2
+
+'''
+
+
+print("""
+mravalstriqoniani
+komentari
+      a = 2.58
+      b = a * 5 - 2 * a
+      d = a ** 2 + b ** 2
+
+""")
---
+++ 2021-2022/VIII კლასი/8_7/003 (10.04.21)/3_1.py
@@ -0,0 +1,61 @@
+# string     <class 'str'>
+a = "Paata mamporia 49 years old"
+print(a)
+print(id(a))
+print(id("Paata mamporia 49 years old"))
+print(type(a))
+
+
+print()
+
+
+# int      <class 'int'>
+a = 49
+print(a)
+print(id(a))
+print(id(49))
+print(type(a))
+
+
+print()
+
+
+# float      <class 'float'>
+a = 4.9
+print(a)
+print(id(a))
+print(id(4.9))
+print(type(a))
+
+
+# ertstriqoniani komentari
+
+"""
+mravalstriqoniani
+komentari
+  dokument striqoni
+
+        x = 2.56
+        y = 3 * x - 1
+        d = x ** 2 + y ** 2 
+"""
+
+'''
+mravalstriqoniani
+komentari
+  dokument striqoni
+
+        x = 2.56
+        y = 3 * x - 1
+        d = x ** 2 + y ** 2 
+'''
+
+print("""
+mravalstriqoniani
+komentari
+  dokument striqoni
+
+        x = 2.56
+        y = 3 * x - 1
+        d = x ** 2 + y ** 2 
+""")
