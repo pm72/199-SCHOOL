6d9dbdce2414605a0382fb8a8d00c2952aa3c72a
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-10 14:41:32 +0400

    pm72

--- 2021-2022/8_3/II სემესტრი/004 (2.10.22)/strings.py
+++ 2021-2022/8_3/II სემესტრი/004 (2.10.22)/strings.py
@@ -1,2 +1,73 @@
 # სტრიქონები  string
 
+# a = "This is a string"
+# print(a)
+#
+# a = 'This is a string'
+# print(a)
+#
+# a = 'd'
+# print(a, type(a))
+#
+# a = "12345"
+# print(a, type(a))
+#
+# a = 12345
+# print(a, type(a))
+
+
+# მრავალსტრიქონიანი სტრიქონები
+intro = "Hello there! \
+My name is Paata mamporia, \
+I am 49 years old. \
+I love Rugby."
+print(intro)
+
+intro = "Hello there! " \
+        "My name is Paata mamporia, " \
+        "I am 49 years old. " \
+        "I love Rugby."
+print(intro)
+
+print()
+
+# '\n'  ახალ სტრიქონზე გადასვლა   End line  New line   ENTER
+# intro = "Hello there!\n\
+# My name is Paata mamporia,\n\
+# I am 49 years old.\n\
+# I love Rugby."
+# print(intro, "\n")
+#
+# intro = """Hello there!
+# My name is Paata mamporia,
+# I am 49 years old.
+# I love Rugby."""
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+# My name is Paata mamporia,
+# I am 49 years old.
+# I love Rugby.'''
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+#             My name is Paata mamporia,
+#                       I am 49 years old.
+#         I love Rugby.'''
+# print(intro, "\n")
+
+
+# ბრჭყალები ბრჭყალებში
+# "Hello", said Susan.
+print('"Hello", said Susan.')
+print("'Hello', said Susan.")
+print("""'Hello', said Susan.""")
+
+# "That's my Teddy", said Pedro.
+print("\"That's my Teddy\", said Pedro.")
+print('"That\'s my Teddy", said Pedro.')
+print(""""That's my Teddy", said Pedro.""")
+
+print("c:\\user\\paata\\strings.py")
+print(r"c:\user\paata\strings.py")
+print(r"c:\user\paata\strings.py\n")
