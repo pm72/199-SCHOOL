7f7266baa53597d320b5eab6d7c2cfdc234e08a7
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-15 14:40:45 +0400

    pm72

--- 2021-2022/7_4/II სემესტრი/005 (2.15.22)/strings.py
+++ 2021-2022/7_4/II სემესტრი/005 (2.15.22)/strings.py
@@ -60,11 +60,68 @@
 
 
 # სტრიქონების კონკატენეაცია
-str1 = "Hello"
-str2 = "there"
-string = str1 + ", " + str2 + "!"
-print(string, "\n")
+# str1 = "Hello"
+# str2 = "there"
+# string = str1 + ", " + str2 + "!"
+# print(string, "\n")
+#
+# # Susan sais "Hi"
+# a = 'Hi!'
+# print('Susan sais "' + a + '"')
 
-# Susan sais "Hi"
-a = 'Hi!'
-print('Susan sais "' + a + '"')
+
+# ცარიელი სტრიქონი
+# a = ''
+# a = ""
+# print(a, type(a))
+
+
+# წვდომა სტრიქონის ელემენტებთან
+# a = 'Hello there!'
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[11], "\n")
+#
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+
+
+# ჭრები სტრიქონში  Slice
+# a = 'Hello there!'
+# print(a[0:4])           # პირველი ოთხი სიმბოლი
+# print(a[:4])            # პირველი ოთხი სიმბოლი
+# print(a[5:9])           # სიმბოლოები მითითებული დიაპაზონით
+# print(a[2:9:3], "\n")   # სიმბოლოები მითითებული დიაპაზონით
+#
+# print(a[-4:])           # ბოლო ოთხი სიმბოლო
+# print(a[6:])
+# print(a[-9: -3])
+
+# a = "Hello there!"
+# print(a, id(a))
+#
+# a = 'T' + a[1:]
+# print(a, id(a))
+#
+# a = 'H' + a[1:]
+# print(a, id(a))
+
+
+# სტრიქონული მეთოდები
+
+#1. სტრიქონის სიგრძე len().   სინტაქსი: len(string)
+# a = "Hello there!"
+# print(len(a))
+
+#2. მეთოდები .capitalize(), .upper(), .lower(), .title()
+# სინტაქსი: string.capitalize(), string.upper()
+# a = 'i am here!'
+# print(a.capitalize())
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+#
+# print()
+# print(a)
