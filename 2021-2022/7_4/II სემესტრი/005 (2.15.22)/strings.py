f3ba223d95b9f3739b025d89cad2626bee575340
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-15 13:03:07 +0400

    pm72

---
+++ 2021-2022/7_4/II სემესტრი/005 (2.15.22)/strings.py
@@ -0,0 +1,70 @@
+# სტრიქონები   string
+
+# a = "This is a string"
+# print(a)
+#
+# numbers = '12345'
+# print(numbers, type(numbers))
+
+
+# მრავალსტრიქონიანი სტრიქონები
+# intro = "Hello there! \
+# My name is Paata Mamporia, \
+# I love Rugby."
+# print(intro)
+#
+# intro = "Hello there! " \
+#         "My name is Paata Mamporia, " \
+#         "I love Rugby"
+# print(intro, "\n")
+#
+# intro = "Hello there!\n\
+# My name is Paata Mamporia,\n\
+# I love Rugby"
+# print(intro, "\n")
+#
+# intro = """Hello there!
+# My name is Paata Mamporia,
+# I love Rugby"""
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+# My name is Paata Mamporia,
+# I love Rugby'''
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+#       My name is Paata Mamporia,
+#             I love Rugby'''
+# print(intro, "\n")
+
+
+# ბრჭყალები ბრჭყალებში
+# # "Hello!", said Susan
+# print('"Hello!", said Susan')
+# print("'Hello!', said Susan")
+#
+# print()
+#
+# # "That's my Taddy", said Pedro.
+# print("\"That's my Taddy\", said Pedro.")
+# print('"That\'s my Taddy", said Pedro.')
+# print(""""That's my Taddy", said Pedro.""")
+# print('''"That's my Taddy", said Pedro.''')
+#
+# print()
+#
+# print("c:\\users\\paata\\strings.py")
+# print(r"c:\users\paata\strings.py")
+# print(r"c:\users\paata\strings.py\n")
+
+
+# სტრიქონების კონკატენეაცია
+str1 = "Hello"
+str2 = "there"
+string = str1 + ", " + str2 + "!"
+print(string, "\n")
+
+# Susan sais "Hi"
+a = 'Hi!'
+print('Susan sais "' + a + '"')
---
+++ 2021-2022/7_5/II სემეტრი/005 (2.15.22)/strings.py
@@ -0,0 +1,57 @@
+# სტრიქონები   string
+
+# a = "This is a string"
+# print(a)
+#
+# a = '1234.23'
+# print(a, type(a))
+
+
+# მრავალსტრიქონიანი სტრიქონები
+# intro = "Hello there! \
+# My name is Paata Mamporia, \
+# I love Rugby."
+# print(intro)
+#
+# intro = "Hello there! " \
+#         "My name is Paata Mamporia, " \
+#         "I love Rugby."
+# print(intro)
+#
+# print()
+#
+# intro = "Hello there!\n\
+# My name is Paata Mamporia,\n\
+# I love Rugby."
+# print(intro, "\n")
+#
+# intro = """Hello there!
+# My name is Paata Mamporia,
+# I love Rugby."""
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+#       My name is Paata Mamporia,
+#               I love Rugby.'''
+# print(intro, "\n")
+
+
+# ბრჭყალები ბრჭყალებში
+# "Hello!", said Susan.
+# print('"Hello!", said Susan.')
+# print("'Hello!', said Susan.")
+#
+# print()
+#
+# # "That's my Teddy", said Pedro
+# print("\"That's my Teddy\", said Pedro")
+# print('"That\'s my Teddy", said Pedro')
+# print(""""That's my Teddy", said Pedro""")
+# print('''"That's my Teddy", said Pedro''')
+
+
+# სტრიქონების კონკატენაცია
+str1 = "Hello"
+str2 = "there"
+string = str1 + " " + str2 + "!"
+print(string)
