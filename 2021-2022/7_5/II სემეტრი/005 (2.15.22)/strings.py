c418d2120425d03a3f72240d256bf2e36b720286
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-15 13:56:29 +0400

    pm72

--- 2021-2022/7_5/II სემეტრი/005 (2.15.22)/strings.py
+++ 2021-2022/7_5/II სემეტრი/005 (2.15.22)/strings.py
@@ -51,7 +51,76 @@
 
 
 # სტრიქონების კონკატენაცია
-str1 = "Hello"
-str2 = "there"
-string = str1 + " " + str2 + "!"
-print(string)
+# str1 = "Hello"
+# str2 = "there"
+# string = str1 + " " + str2 + "!"
+# print(string)
+#
+# # Susan sais "Hi!"
+# a = 'Hi!'
+# print('Susan sais "' + a + '"')
+# print('Bob sais "' + a + '", How are you?')
+
+
+# ცარიელი სტრიქონი
+# a = ''
+# a = ""
+# print(a)
+
+
+# წვდომა სტრიქონის ელემენტებთან
+# a = 'Hello there!'
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[11], "\n")
+#
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+
+
+# ჭრები სტრიქონში  Slice
+# a = 'Hello there!'
+# print(a[0:4])       # პირველი ოთხი სიმბოლო
+# print(a[:4])        # პირველი ოთხი სიმბოლოდ
+# print(a[5:9], "\n") # სიმბოლოები მოცემულ დიაპაზონში
+#
+# print(a[-4:])       # ბოლო ოთხი სიმბოლო
+# print(a[-8: -3])
+
+# a = "Hello there!"
+# print(a, id(a))
+#
+# a = 'P' + a[1:]
+# print(a, id(a))
+#
+# a = 'H' + a[1:]
+# print(a, id(a))
+
+
+# სტრიქონული მეთოდები
+
+#1. სტრიქონის სიგრძე    len()  ===>  len(string)
+# a = "Hello there!"
+# print(len(a))
+
+#2. მეთოდები .capitalize(), .upper(), .lower(), .title()
+# string.capitalize()  string.upper()
+a = "i am here!"
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+print(a.title())
+
+print()
+print(a)
+
+
+#3. მეთოდი .count()  ===>  string.count(word)
+a = '''Susan is a lovely girl.
+Burky is Susan's friend.
+Burky plays with Susan'''
+print(a.count('Susan'))
+print(a.count('Burky'))
+print(a.count('burky'))
