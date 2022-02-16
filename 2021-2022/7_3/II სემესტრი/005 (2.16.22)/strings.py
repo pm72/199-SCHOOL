ae8e773046b014d61c97e86d3b9631cfe4ade5e9
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-16 13:15:38 +0400

    pm72

---
+++ 2021-2022/7_3/II სემესტრი/005 (2.16.22)/strings.py
@@ -0,0 +1,74 @@
+# სტრიქონები   string
+
+# a = "This is a string"
+# print(a)
+#
+# a = "d"
+# print(a, type(a))
+#
+# a = 'd'
+# print(a, type(a))
+#
+# a = '123456'
+# print(a, type(a))
+#
+# a = 123456
+# print(a, type(a))
+#
+# a = 1234.56
+# print(a, type(a))
+
+
+# მრავალსტრიქონიანი სტრიქონები
+# intro = "Hello there! \
+# My name is Paata Mamporia, \
+# I love Rugby."
+# print(intro)
+#
+# intro = "Hello there! " \
+#         "My name is Paata Mamporia, " \
+#         "I love Rugby."
+# print(intro)
+#
+# print()
+#
+# # '\n'  ===> ახალ ხაზზე გადასვლა  ENTER
+# intro = "Hello there!\n\
+# My name is Paata Mamporia,\n\
+# I love Rugby."
+# print(intro, "\n")
+#
+# intro = """Hello there!
+# My name is Paata Mamporia,
+# I love Rugby."""
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+# My name is Paata Mamporia,
+# I love Rugby.'''
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+# My name is Paata Mamporia,
+# I love Rugby.'''
+# print(intro, "\n")
+#
+# intro = '''Hello there!
+#     My name is Paata Mamporia,
+#                 I love Rugby.'''
+# print(intro, "\n")
+
+
+# ბრჭყალები ბრჭყალებში
+# "Hello", said Susan
+print("\"Hello\", said Susan")
+print('"Hello", said Susan')
+print("'Hello', said Susan")
+
+# "That's my Teddy", said Pedro
+print("\"That's my Teddy\", said Pedro")
+print('"That\'s my Teddy", said Pedro')
+
+print("c:\\user\\paata\\strings.py")
+print(r"c:\user\paata\strings.py")
+print(r"c:\user\paata\strings.py\n")
