a82688ea885599403670c676f985dbf94e834e74
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-16 14:01:03 +0400

    pm72

--- 2021-2022/7_3/II სემესტრი/005 (2.16.22)/strings.py
+++ 2021-2022/7_3/II სემესტრი/005 (2.16.22)/strings.py
@@ -61,14 +61,48 @@
 
 # ბრჭყალები ბრჭყალებში
 # "Hello", said Susan
-print("\"Hello\", said Susan")
-print('"Hello", said Susan')
-print("'Hello', said Susan")
+# print("\"Hello\", said Susan")
+# print('"Hello", said Susan')
+# print("'Hello', said Susan")
+#
+# # "That's my Teddy", said Pedro
+# print("\"That's my Teddy\", said Pedro")
+# print('"That\'s my Teddy", said Pedro')
+#
+# print("c:\\user\\paata\\strings.py")
+# print(r"c:\user\paata\strings.py")
+# print(r"c:\user\paata\strings.py\n")
 
-# "That's my Teddy", said Pedro
-print("\"That's my Teddy\", said Pedro")
-print('"That\'s my Teddy", said Pedro')
 
-print("c:\\user\\paata\\strings.py")
-print(r"c:\user\paata\strings.py")
-print(r"c:\user\paata\strings.py\n")
+# სტრიქონების მიბმა '+' ოპერატორით (კონკატენაცია)
+# str1 = 'Hello'
+# str2 = 'there'
+# string = str1 + ' ' + str2 + "!"
+# print(string)
+#
+# string = str2 + ' ' + str1 + "!"
+# print(string)
+#
+# # Susan sais "Hi"
+# a = 'Hi'
+# print('Susan sais "' + a + '"!')
+# print('Bob sais "' + a + '"!')
+
+
+# სტრიქონული მეთოდები
+
+#1. სტრიქონის სიგრძე len(). სინტაქსი: len(string)
+# a = "Hello there!"
+# print("Length of string:", len(a))
+# print("Length of typed string:", len("12354 is string's numbers"))
+
+#2. მეთოდები .capitalize(), .upper(), .lower(), .title()
+# სინტაქსი: string.capitalize(), string.upper()
+a = 'i am heRE!'
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+print(a.title())
+
+print()
+print(a)
