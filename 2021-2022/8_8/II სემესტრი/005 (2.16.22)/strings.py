cc8bd412485d96db1df06f4a9211949e47f5b87b
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-16 15:33:00 +0400

    pm72

--- 2021-2022/8_8/II სემესტრი/005 (2.16.22)/strings.py
+++ 2021-2022/8_8/II სემესტრი/005 (2.16.22)/strings.py
@@ -77,17 +77,64 @@
 
 
 # სტრიქონების კონკატენაცია
-str1 = "Hello"
-str2 = "there!"
-string = str1 + " " + str2
-print(string)
+# str1 = "Hello"
+# str2 = "there!"
+# string = str1 + " " + str2
+# print(string)
+#
+# string = str2 + " " + str1
+# print(string)
+#
+# print()
+#
+# # Susan sais "Hi"!
+# a = "Hi"
+# print('Susan sais "' + a + '"' + "!")
+# print('Susan sais "' + a + "\"!")
 
-string = str2 + " " + str1
-print(string)
 
-print()
+# წვდომა სტრიქონის ელემენტებზე
+# a = 'Hello there!'
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[11], "\n")
+#
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+# print(a[-4])
 
-# Susan sais "Hi"!
-a = "Hi"
-print('Susan sais "' + a + '"' + "!")
-print('Susan sais "' + a + "\"!")
+
+# ჭრები სტრიქონში Slice. სინტაქსი: string[start:stop:step]
+# a = 'Hello there!'
+# print(a[0:4])       # პირველი ოთხი სიმბოლო
+# print(a[:4])        # პირველი ოთხი სიმბოლო
+# print(a[5:9])       # სიმბოლოები მითითებული დიაპაზონით
+# print(a[2:9:3])     # სიმბოლოები მითითებული დიაპაზონით
+# print(a[-4:])       # ბოლო ოთხი სიმბოლო
+# print(a[3:])
+# print(a[3::4])
+# print(a[::3])
+
+# a = "Hello there!"
+# print(a, id(a))
+#
+# a = 'P' + a[1:]
+# print(a, id(a))
+#
+# a = 'H' + a[1:]
+# print(a, id(a))
+
+
+a = "Hello there!"
+# b = "Hello there!"
+# print(a, id(a))
+# print(b, id(b))
+
+b = a
+
+a = 'P' + a[1:]
+print(a)
+print(b)
