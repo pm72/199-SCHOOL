0712864552afa34464bd8552089c034f6128a53f
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-17 14:46:32 +0400

    pm72

--- 2021-2022/8_3/II სემესტრი/005 (2.17.22)/strings.py
+++ 2021-2022/8_3/II სემესტრი/005 (2.17.22)/strings.py
@@ -17,19 +17,19 @@
 
 
 # მრავალსტრიქონიანი სტრიქონები
-intro = "Hello there! \
-My name is Paata mamporia, \
-I am 49 years old. \
-I love Rugby."
-print(intro)
-
-intro = "Hello there! " \
-        "My name is Paata mamporia, " \
-        "I am 49 years old. " \
-        "I love Rugby."
-print(intro)
-
-print()
+# intro = "Hello there! \
+# My name is Paata mamporia, \
+# I am 49 years old. \
+# I love Rugby."
+# print(intro)
+#
+# intro = "Hello there! " \
+#         "My name is Paata mamporia, " \
+#         "I am 49 years old. " \
+#         "I love Rugby."
+# print(intro)
+#
+# print()
 
 # '\n'  ახალ სტრიქონზე გადასვლა   End line  New line   ENTER
 # intro = "Hello there!\n\
@@ -59,15 +59,65 @@ print()
 
 # ბრჭყალები ბრჭყალებში
 # "Hello", said Susan.
-print('"Hello", said Susan.')
-print("'Hello', said Susan.")
-print("""'Hello', said Susan.""")
+# print('"Hello", said Susan.')
+# print("'Hello', said Susan.")
+# print("""'Hello', said Susan.""")
+#
+# # "That's my Teddy", said Pedro.
+# print("\"That's my Teddy\", said Pedro.")
+# print('"That\'s my Teddy", said Pedro.')
+# print(""""That's my Teddy", said Pedro.""")
+#
+# print("c:\\user\\paata\\strings.py")
+# print(r"c:\user\paata\strings.py")
+# print(r"c:\user\paata\strings.py\n")
 
-# "That's my Teddy", said Pedro.
-print("\"That's my Teddy\", said Pedro.")
-print('"That\'s my Teddy", said Pedro.')
-print(""""That's my Teddy", said Pedro.""")
 
-print("c:\\user\\paata\\strings.py")
-print(r"c:\user\paata\strings.py")
-print(r"c:\user\paata\strings.py\n")
+# ცარიელი სტრიქონი
+# a = ''
+# a = ""
+# print(a)
+
+
+# სტრიქონების კონკატენაცია (შეწებება '+' ოპერატორით)
+# str1 = "Hello"
+# str2 = "there"
+# string = str1 + " " + str2 + '!'
+# print(string)
+#
+# # Suasan says "Hi"!
+# a = 'Hi'
+# print('Susan says "' + a + '"!')
+# print('Daniel says "' + a + '"!')
+
+
+# წვდომა სტრიქონის ელემენტებზე (სიმბოლოებზე)
+# a = 'Hello there!'
+# # print(a[0])
+# # print(a[1])
+# # print(a[2])
+# # print(a[11], "\n")
+# #
+# # print(a[-1])
+# # print(a[-2])
+# # print(a[-3])
+
+
+# ჭრები სტრიქონში Slice. სინტაქსი: string[start:stop:step]
+# a = 'Hello there!'
+# print(a[0:4])           # პირველი ოთხი სიმბოლო
+# print(a[:4])            # პირველი ოთხი სიმბოლო
+# print(a[2:7])           # სიმბოლოები მითითებული დიაპაზონით
+# print(a[2:9:3])         # სიმბოლოები მითითებული დიაპაზონით
+# print(a[-4:])           # ბოლო ოთხი სიმბოლო
+# print(a[3:])
+
+
+a = 'Hello there!'
+print(a, id(a))
+
+a = 'P' + a[1:]
+print(a, id(a))
+
+a = 'H' + a[1:]
+print(a, id(a))
