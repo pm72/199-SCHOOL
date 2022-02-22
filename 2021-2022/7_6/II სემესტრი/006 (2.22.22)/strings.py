1e04a521103e6bf84a1d2516ddee88765042eaf7
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-22 15:28:41 +0400

    pm72

--- 2021-2022/7_6/II სემესტრი/006 (2.22.22)/strings.py
+++ 2021-2022/7_6/II სემესტრი/006 (2.22.22)/strings.py
@@ -120,11 +120,67 @@
 
 #2. მეთოდები: .capitalize(), .upper(), .lower(), .title()
 # სინტაქსი: string.capitalize(), string.upper()
-a = 'i am there!'
-print(a.capitalize())
-print(a.upper())
-print(a.lower())
-print(a.title())
+# a = 'i am there!'
+# print(a.capitalize())
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+#
+# print()
+# print(a)
 
-print()
-print(a)
+
+#3. .count()
+# სინტაქსი: string.count(word)
+# a = '''Susan is a lovely girl.
+# Barky is Susan's best friend.
+# Barky plays with Susan...'''
+# print(a.count('Susan'))
+# print(a.count('Barky'))
+# print(a.count('susan'))
+# print(a.count("Susan'"))
+# print(a.count('Susan\''))
+
+
+#4. .strip(), .lstrip(), .rstrip()
+# a = '     I love coding. I have fun with Python coding.       '
+# a = a.strip()
+# print(a)
+
+
+#5. .replace(), .find(). index()
+# a = '''Susan is a lovely girl.
+# Barky is Susan's best friend.
+# Barky plays with Susan...'''
+# a = a.replace('Susan', 'Ronny')
+# print(a)
+
+# a = 'I love coding. I have fun with Python coding.'
+# print(a.find('coding'))
+# print(a.find('Coding'), "\n")
+#
+# print(a.index('coding'))
+# print(a.index('Coding'))
+
+
+#6. .split()
+# სინტაქსი: string.split(separator, limit)
+# a = "I love coding."
+# print(a.split())   # separator=' ' and separator='\n' and separator='\t'
+#
+# a = "I love coding.\nI have fun with\t\t\tPython coding."
+# print(a.split())   # separator=' ' and separator='\n' and separator='\t'
+#
+# a = "I love coding.\nI have fun with\t\t\tPython coding."
+# print(a.split('.'))   # separator=' ' and separator='\n' and separator='\t'
+#
+# a = "I love coding.\nI have fun with\t\t\tPython coding."
+# print(a.split('.\n'))   # separator=' ' and separator='\n' and separator='\t'
+#
+# # 3+5= 7, 6, 8, 9
+# a = "3+5=;7;6;8;9"
+# print(a.split(';'))
+
+
+#7. ოპერატორი 'in'   ======> True / False (ჭეშმარიტი / მცდარი)
+a = ""
