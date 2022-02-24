f18ae1a5701fd2ca10434b49642b9324ccb0d8dc
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-24 15:27:29 +0400

    pm72

--- 2021-2022/7_1/II სემესტრი/006 (2.24.22)/strings.py
+++ 2021-2022/7_1/II სემესტრი/006 (2.24.22)/strings.py
@@ -114,12 +114,85 @@
 # print(a[-4:])           # ბოლო ოთხი სიმბოლო
 # print(a[3:])
 
-a = "Hello there!"
-print(a, id(a))
+# a = "Hello there!"
+# print(a, id(a))
+#
+# a = 'P' + a[1:]
+# print(a, id(a))
+#
+# a = 'H' + a[1:]
+# print(a, id(a))
+# print(a[1:], id(a[1:]))
 
-a = 'P' + a[1:]
-print(a, id(a))
 
-a = 'H' + a[1:]
-print(a, id(a))
-print(a[1:], id(a[1:]))
+# სტრიქონული მეთოდები
+
+#1. სტრიქონის სიგრძე (სიმბოლოების რაოდენობა)  len()
+# a = "Hello there!s"
+# print(len(a))
+
+
+#2. .capitalize(), .upper(), .lower(), .title()
+# სინტაქსი: string.capitalize(), string.upper(), ...
+# a = "i am hErE."
+# print(a.capitalize())
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+
+
+#3. .count()
+# სინტაქსი: string.count(word)
+# a = '''Susan is a lovely girl.
+# Barky is Susan's best friend.
+# Barky plays with Susan...'''
+# print(a.count('Susan'))
+# print(a.count('Barky'))
+# print(a.count('barky'))
+# print(a.count('susan'))
+# print(a.count('paata'))
+
+
+#4. .strip(), .lstrip(), .rstrip()
+# a = "      Hello there!   "
+# a = a.strip()
+# print(a)
+
+
+#5. .replace(), .find(), .index()
+# a = '''Susan is a lovely girl.
+# Barky is Susan's best friend.
+# Barky plays with Susan...'''
+#
+# a = a.replace('Susan', 'Ronny')
+# print(a)
+
+# a = 'I cyan love coding. I have fun with coding.'
+# print(a.find('coding'))
+# print(a.find('c'))
+# print(a.find('Coding'), "\n")
+#
+# print(a.index('coding'))
+# print(a.index('c'))
+# print(a.index('Coding'), "\n")
+
+
+# .split()
+# სინტაქსი: string.split(separator, maxsplit)
+# a = "I love coding."
+# print(a.split())    # separator=' ' or separator='\n' or separator='\t', maxsplit=-1
+#
+# a = "I love coding.\nI have fun with\t\t\t\tcoding."
+# print(a.split())    # separator=' ' or separator='\n' or separator='\t', maxsplit=-1
+#
+# a = "I love coding.\nI have fun with\t\t\t\tcoding."
+# print(a.split('.'))    # separator=' ' or separator='\n' or separator='\t', maxsplit=-1
+#
+# a = "I love coding.\nI have fun with\t\t\t\tcoding."
+# print(a.split('.\n'))    # separator=' ' or separator='\n' or separator='\t', maxsplit=-1
+
+a = "5+8=;15;18;13;14"
+print(a)
+print(a.split())
+print(a.split(';'))
+print(a.split(';')[3])
