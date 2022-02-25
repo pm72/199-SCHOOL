a635262e1ef72a4bd4261f63f13cc29f0abca513
pm72 <46081859+pm72@users.noreply.github.com>
2022-02-25 15:30:18 +0400

    pm72

--- 2021-2022/8_2/II სემესტრი/006 (2.25.22)/strings.py
+++ 2021-2022/8_2/II სემესტრი/006 (2.25.22)/strings.py
@@ -104,14 +104,101 @@
 
 
 # ჭრები სტიქონში Slice. სინტაქსი: string[start:stop-1:step=1]
-a = 'Hello there!'
-print(a[0:4:1])
-print(a[0:4])
-print(a[:4])
-print(a[2:7])
-print(a[2:9:3])
-print(a[3:])
-print(a[-4:])
-print(a[-9::3])
-print(a[2::3])
-print(a[::3])
+# a = 'Hello there!'
+# print(a[0:4:1])
+# print(a[0:4])
+# print(a[:4])
+# print(a[2:7])
+# print(a[2:9:3])
+# print(a[3:])
+# print(a[-4:])
+# print(a[-9::3])
+# print(a[2::3])
+# print(a[::3])
+
+
+
+# სტრიქონული მეთოდები
+
+#1. სტრიქონის სიგრძე (სიმბოლოების რაოდენობა)  len()
+# a = "Hello world!"
+# print(len(a))
+
+
+#2. .capitalize(), .upper(), .lower(), .title()
+# a = 'i am hErE.'
+# print(a.capitalize())
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+# a = a.lower()
+# print(a)
+
+
+#3. .count()
+# სინტაქსი: string.count(word)
+# a = '''Susan is a lovely girl.
+# Barky is Susan's best friend.
+# Barky plays with Susan...'''
+# print(a.count('Susan'))
+# print(a.count('Barky'))
+# print(a.count('barky'))
+# print(a.count('susan'))
+# print(a.count('Jeck'))
+
+
+#4. .strip(), .lstrip(), .rstrip()
+# a = '              Hello there!      '
+# print(a.strip())
+#
+# a = '*********Hello there!****'
+#
+# # a = a.strip('*')
+# # print(a)
+#
+# a = a.rstrip('*')
+# print(a)
+
+
+#5. .replace(), .finf(), .index()
+# a = '''Susan is a lovely girl.
+# Barky is Susan's best friend.
+# Barky plays with Susan...'''
+#
+# a = a.replace('Susan', 'Ronny', 2)
+# print(a)
+
+# a = 'I cyan love coding. I have fun with coding.'
+# print(a.find('coding'))
+# print(a.find('c'))
+# print(a.find('Coding'))
+#
+# print()
+#
+# print(a.index('Coding'))
+# print(a.index('coding'))
+# print(a.index('c'))
+
+
+#6. .split()
+# სინტაქსი: string.split(separator, maxsplit)
+a = "I love coding."
+print(a.split())   # separatot=' ' or separator='\n' or separator='\t', maxsplit=-1
+
+a = "I love coding.\nI have fun with\t\t\tcoding."
+print(a.split())   # separatot=' ' or separator='\n' or separator='\t', maxsplit=-1
+
+a = "I love coding.\nI have fun with\t\t\tcoding."
+print(a.split('.'))   # separatot=' ' or separator='\n' or separator='\t', maxsplit=-1
+
+a = "I love coding.\nI have fun with\t\t\tcoding."
+print(a.split('.\n'))   # separatot=' ' or separator='\n' or separator='\t', maxsplit=-1
+
+a = "numbers*123*numbers*456*numbers*789"
+print(a.split('*'))
+
+a = "numbers*123*numbers*456*numbers*789"
+print(a.split('*', 1))
+
+a = "numbers*123*numbers*456*numbers*789"
+print(a.split('*',2))
