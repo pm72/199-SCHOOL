c8533fd8120bb875120f2c8ecbc0edc9bb09c640
pm72 <46081859+pm72@users.noreply.github.com>
2022-03-21 14:42:56 +0400

    pm72

---
+++ 2021-2022/8_6/II სემესტრი/007 (3.21.22)/input.py
@@ -0,0 +1,17 @@
+# a = input("Enter a number: ")
+# print(f"Your enter {a}")
+
+# x = input("Enter first number: ")
+# y = input("Enter second number: ")
+# print(f"The sum is {x + y}")
+# print(type(x), type(y))
+
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+# print(f"The sum is {x + y}")
+# print(type(x), type(y))
+
+x = eval(input("Enter first number: "))
+y = eval(input("Enter second number: "))
+print(f"The sum is {x + y}")
+print(type(x), type(y))
