3873ff8c56d75d67b1cc4319731498a706c82035
pm72 <46081859+pm72@users.noreply.github.com>
2022-04-01 13:13:40 +0400

    pm72

---
+++ 2021-2022/8_2/II სემესტრი/008 (4.1.22)/chess_players_print.py
@@ -0,0 +1,1 @@
+from chess_players import players
---
+++ 2021-2022/8_5/II სემესტრი/008 (4.1.22)/chess_players.py
@@ -0,0 +1,37 @@
+players = {
+  'Jobava': {
+    'country': 'Georgia',
+    'rating': 2588,
+    'age': 38
+  },
+  'Caruana': {
+    'country': 'USA',
+    'rating': 2781,
+    'age': 29
+  },
+  'Giri': {
+    'country': 'Netherlands',
+    'rating': 2771,
+    'age': 27
+  },
+  'Mamedyarov': {
+    'country': 'Azerbaijan',
+    'rating': 2776,
+    'age': 36
+  },
+  'Carlsen': {
+    'country': 'Norway',
+    'rating': 2864,
+    'age': 31
+  },
+  'Ding': {
+    'country': 'China',
+    'rating': 2799,
+    'age': 29
+  },
+  'Karjakin': {
+    'country': 'Russia',
+    'rating': 2747,
+    'age': 32
+  },
+}
