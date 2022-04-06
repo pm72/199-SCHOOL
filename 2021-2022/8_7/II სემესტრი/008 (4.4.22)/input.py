be6766bf550447e4c50cc398a9caf979d45c9ae0
pm72 <46081859+pm72@users.noreply.github.com>
2022-04-06 13:12:51 +0400

    pm72

---
+++ 2021-2022/8_7/II სემესტრი/008 (4.4.22)/input.py
@@ -0,0 +1,14 @@
+# name = input("Enter your name: ")
+# print(name)
+
+# x = input("Enter a first number: ")
+# y = input("Enter a second number: ")
+# print(f"The sum is {x + y}")
+# print(type(x), type(y))
+
+
+# ტიპების გარდაქმნა
+x = int(input("Enter a first number: "))
+y = int(input("Enter a second number: "))
+print(f"The sum is {x + y}")
+print(type(x), type(y))
