22272c47c83e657b48d0d87c0403ed17f5b40834
pm72 <46081859+pm72@users.noreply.github.com>
2022-04-07 14:44:11 +0400

    pm72

---
+++ 2021-2022/8_3/II სემესტრი/009 (4.7.22)/num_input.py
@@ -0,0 +1,33 @@
+# x = input("Number one: ")         # input() ფუნქცია აბრუნებს ატრიქონს  type str
+# y = input("Number two: ")
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+# x = int(input("Number one: "))
+# y = int(input("Number two: "))
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+# x = float(input("Number one: "))
+# y = float(input("Number two: "))
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+# x = eval(input("Number one: "))
+# y = eval(input("Number two: "))
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+a = "6 * 9.0124"
+print(a)
+print(eval(a))
+
+print("Welcome", eval("input('Your name: ')") + "!")
