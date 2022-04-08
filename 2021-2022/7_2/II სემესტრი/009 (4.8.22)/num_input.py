ba8df7261192bc9395d9e0e9ca24e28d49e5b94c
pm72 <46081859+pm72@users.noreply.github.com>
2022-04-08 13:58:24 +0400

    pm72

---
+++ 2021-2022/7_2/II სემესტრი/009 (4.8.22)/num_input.py
@@ -0,0 +1,38 @@
+# x = input("Number1: ")        # input() ფუნქცია აბრუნებს სტრიქონს str
+# y = input("Number2: ")
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+
+# x = int(input("Number1: "))
+# y = int(input("Number2: "))
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+
+# x = float(input("Number1: "))
+# y = float(input("Number2: "))
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+
+# x = eval(input("Number1: "))
+# y = eval(input("Number2: "))
+# add = x + y
+#
+# print(f"The sum is {x} + {y} = {add}")
+# print(type(x), type(y), type(add))
+
+
+print("3 * 6")
+print(eval("3 * 6"))
+print(eval("45 + (6.98 + 2**3.56) / 2.54 - (5**1.24 + 1.054 ** 0.69) / (3**1.5 +2.36)"))
+print(45 + (6.98 + 2**3.56) / 2.54 - (5**1.24 + 1.054 ** 0.69) / (3**1.5 +2.36))
+
+print(eval('2.56 * 6 + 1fg'))
---
+++ 2021-2022/7_2/II სემესტრი/009 (4.8.22)/text_input.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+number = input("Enter your favorite number: ")
+
+greeting = f"Welcome {name}!"
+message = f"{name}, your entered favorite number is {number}."
+
+print(f"{greeting}\n{message}")
+
+
+# ==================
+# ==================
+# ==================
+# ==================
+print()
+
+name = input("Enter your name: ")
+number = input("Enter your favorite number: ")
+print(f"{greeting}\n{message}")
