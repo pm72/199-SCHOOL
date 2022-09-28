8f15bfad59b7033c0e52d5a9ac46f1aef28787ef
pm72 <46081859+pm72@users.noreply.github.com>
2022-09-28 12:45:24 +0400

    Create წრის_ფართობის_გამოთვლა.py

---
+++ 2022-2023/7_7/22.9.27/წრის_ფართობის_გამოთვლა.py
@@ -0,0 +1,24 @@
+# s = pi * r ** 2
+# pi = 3.14159...
+
+radius = eval(input("Enter radius: "))
+PI = 3.14159
+
+area = PI * radius ** 2
+length = 2 * PI * radius
+
+print("The area of the circle with radius", radius, "is", area)
+print("The length of the circle with radius", radius, "is", length)
+
+
+
+
+input()
+
+
+
+
+# string    str    "paata" 'tea'  'C'  "201"  "@#!dfrd dgt"
+# numeric   int, float   int: 25, -9, 0 12 ...   float: 1.58 -0.036, 1.0
+
+# '3'  int('3') = 3   float('1.69') = 1.69   eval('3') = 3  eval('0.25') = 0.25
