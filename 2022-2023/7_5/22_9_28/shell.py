b4e4a06595b406520a125337fa5e378b69303738
pm72 <46081859+pm72@users.noreply.github.com>
2022-09-28 12:38:52 +0400

    Add files via upload

---
+++ 2022-2023/7_5/22_9_28/shell.py
@@ -0,0 +1,61 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2+3
+5
+>>> (2.59 + 1.32) / (0.259 - 1.58) ** 2.69
+(-1.0393191282398102-1.5293116560818454j)
+>>> (2.59 + 1.32) / (0.259 + 1.58) ** 2
+1.1561476450810058
+>>> x = 15
+>>> y = 3.6
+>>> 
+>>> x * (y + 2)
+84.0
+>>> 
+=========== RESTART: D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py ===========
+Traceback (most recent call last):
+  File "D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py", line 6, in <module>
+    area = pi * radius ** 2
+NameError: name 'pi' is not defined
+>>> 
+=========== RESTART: D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py ===========
+>>> area.
+SyntaxError: invalid syntax
+>>> area
+1256.636
+>>> radius
+20
+>>> 
+=========== RESTART: D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py ===========
+Traceback (most recent call last):
+  File "D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py", line 8, in <module>
+    length = 2 * PI * r
+NameError: name 'r' is not defined
+>>> 
+=========== RESTART: D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py ===========
+1256.636
+125.6636
+>>> 
+=========== RESTART: D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py ===========
+The area of the citcle is 1256.636
+The length of the citcle is 125.6636
+>>> 
+=========== RESTART: D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py ===========
+The area of the citcle with radius 20 is 1256.636
+The length of the citcle with radius 20 is 125.6636
+>>> 
+=========== RESTART: D:/199/7_5/22.9.28/წრის ფართობის გამოთვლა.py ===========
+The area of the citcle with radius 20.58 is 1330.5777188759998
+The length of the citcle with radius 20.58 is 129.3078444
+>>> 
+>>> 
+>>> '1' + 5
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    '1' + 5
+TypeError: must be str, not int
+>>> '1' + '5'
+'15'
+>>> 1+5
+6
+>>> 
