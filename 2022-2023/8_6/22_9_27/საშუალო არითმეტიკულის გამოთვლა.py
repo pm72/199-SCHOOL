cfd4325bb6bce1ba1dd518a6a10fa9a3dba0dc69
pm72 <46081859+pm72@users.noreply.github.com>
2022-09-28 13:55:30 +0400

    Add files via upload

---
+++ 2022-2023/8_6/22_9_27/საშუალო არითმეტიკულის გამოთვლა.py
@@ -0,0 +1,9 @@
+##numb1 = eval(input("Enter the first number: "))
+##numb2 = eval(input("Enter the second number: "))
+##numb3 = eval(input("Enter the third number: "))
+
+numb1, numb2, numb3 = eval(input("Enter three numbers separated by comas: "))
+
+average = (numb1 + numb2 + numb3) / 3
+
+print("Average value is", round(average, 2))
---
+++ 2022-2023/8_6/22_9_27/წრის_ფართობის_გამოთვლა.py
@@ -0,0 +1,25 @@
+# s = pi * r ** 2
+# pi = 3.14159...
+
+radius = eval(input("Enter the radius: "))
+PI = 3.14159
+
+area = PI * radius ** 2
+length = 2 * PI * radius
+
+print("The area of the circle with radius", radius, "is", area)
+print("The length of the circle with radius", radius, "is", length)
+
+
+input()
+
+
+
+
+##   string   str   "paata"  'roma'  "s"  '2'  "2589"  '1'
+##   numeric: int, float
+##   int     25  89 -6 0 -125 1 ...
+##   float   1.58  -0.0536  5.98  1.0 ...
+
+##   int('5') = 5   float('1.56') = 1.56 float('1') = 1.0
+##   eval('5') = 5  eval('1.56') = 1.56
