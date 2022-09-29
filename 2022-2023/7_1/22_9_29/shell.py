daf88625a022943e2aab01955a7ded973a1ac176
pm72 <46081859+pm72@users.noreply.github.com>
2022-09-29 09:18:27 +0400

    Add files via upload

---
+++ 2022-2023/7_1/22_9_29/shell.py
@@ -0,0 +1,65 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+>>> area
+1256.636
+>>> radius
+20
+>>> PI
+3.14159
+>>> perimeter
+125.6636
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+1256.636
+125.6636
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1256.636
+125.6636
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1256.636
+The perimeter of the circle is 125.6636
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1256.636 of radius 20
+The perimeter of the circle is 125.6636 of radius 20
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1330.5777188759998 of radius 20.58
+The perimeter of the circle is 129.3078444 of radius 20.58
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1331 of radius 20.58
+The perimeter of the circle is 129.3078444 of radius 20.58
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1330.58 of radius 20.58
+The perimeter of the circle is 129.3078444 of radius 20.58
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1330.6 of radius 20.58
+The perimeter of the circle is 129.3078444 of radius 20.58
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+The area of the circle is 1330.58 of radius 20.58
+The perimeter of the circle is 129.31 of radius 20.58
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+36
+Traceback (most recent call last):
+  File "D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py", line 11, in <module>
+    area = PI * radius ** 2
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> 
+=========== RESTART: D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py ===========
+Entrer radius: 25
+Traceback (most recent call last):
+  File "D:/199/7_1/22.9.29/წრის ფართობის გამოთვლა.py", line 11, in <module>
+    area = PI * radius ** 2
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> 
