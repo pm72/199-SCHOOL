d8d893d36a59bb10948fb1769fad1750d8a8608d
pm72 <46081859+pm72@users.noreply.github.com>
2022-10-06 10:02:58 +0400

    Add files via upload

---
+++ 2022-2023/7_4/22.10.06/shell.py
@@ -0,0 +1,113 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: D:\199\7_4\22.10.06\sales_tax.py =================
+Enter a purshuse amount: 569.24
+Enter a tax %: 20
+Sales Tax is 113.84800000000001
+>>> 
+================= RESTART: D:\199\7_4\22.10.06\sales_tax.py =================
+Enter a purshuse amount: 569
+Enter a tax %: 5
+Sales Tax is 28 2
+>>> 
+================= RESTART: D:\199\7_4\22.10.06\sales_tax.py =================
+Enter a purshuse amount: 569
+Enter a tax %: 7.5
+Sales Tax is 42.67
+>>> 
+================= RESTART: D:\199\7_4\22.10.06\sales_tax.py =================
+Enter a purshuse amount: 269
+Enter a tax %: 7.5
+Sales Tax is 20.18
+Total sale is 289.18
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+<built-in function time>
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+1665034841.1395953
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+1665034936.6535027
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+1665034939.4734266
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+1665035022
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+27750585.283333335
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+27750585
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+52
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+Current time: 5:47:31 GMT
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+Current time: 9:48:38 GMT
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+Current time: 9:48:54 GMT
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+Current time: 9:48:58 GMT
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+Current time: 9:49:2 GMT
+>>> 500 % 60
+20
+>>> 500 // 60
+8
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+1665035525
+Current time: 9:52:5 GMT
+>>> int(time()) % 60
+37
+>>> minutes =  int(time()) // 60
+>>> minutes
+27750593
+>>> current_minutes = minutes % 60
+>>> current_minutes
+53
+>>> hours = current_minutes % 60
+>>> hours
+53
+>>> hours current_minutes // 60
+SyntaxError: invalid syntax
+>>> hours = current_minutes // 60
+>>> hors
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    hors
+NameError: name 'hors' is not defined
+>>> hours
+0
+>>> current_minutes
+53
+>>> hours = int(time()) % 60 // 60
+>>> hours
+0
+>>> hours
+0
+>>> int(time())
+1665035809
+>>> int(time()) % 60
+57
+>>> hours = int(time()) // 60
+>>> hours
+27750597
+>>> hours = hours % 24
+>>> hours
+21
+>>> 
+================ RESTART: D:/199/7_4/22.10.06/current_time.py ================
+1665035916
+Current time: 9:58:36 GMT
+>>> 
