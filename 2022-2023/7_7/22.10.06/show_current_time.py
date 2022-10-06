0a9919d41edc55a110fec07d1240a297b3ef7cd9
pm72 <46081859+pm72@users.noreply.github.com>
2022-10-06 14:18:21 +0400

    Add files via upload

---
+++ 2022-2023/7_7/22.10.06/show_current_time.py
@@ -0,0 +1,24 @@
+import time
+
+currentTime = time.time() # Get current time
+
+# Obtain the total seconds since midnight, Jan 1, 1970
+totalSeconds = int(currentTime)
+
+# Get the current second
+currentSecond = totalSeconds % 60
+
+# Obtain the total minutes
+totalMinutes = totalSeconds // 60
+
+# Compute the current minute in the hour
+currentMinute = totalMinutes % 60
+
+# Obtain the total hours
+totalHours = totalMinutes // 60
+
+# Compute the current hour
+currentHour = totalHours % 24
+
+# Display results
+print(f"Current time is {currentHour+4}:{currentMinute}:{currentSecond} GMT")
