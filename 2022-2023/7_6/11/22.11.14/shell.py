ffe1d0355b947ce6cdd92420b32d2c7c76421826
pm72 <46081859+pm72@users.noreply.github.com>
2022-11-14 12:58:05 +0400

    pm72

---
+++ 2022-2023/7_6/11/22.11.14/shell.py
@@ -0,0 +1,180 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("He said, "John's program is easy to read"")
+      
+SyntaxError: unterminated string literal (detected at line 1)
+
+
+'\b'
+      
+'\x08'
+
+"\""
+      
+'"'
+print("He said, \"John's program is easy to read\"")
+      
+He said, "John's program is easy to read"
+
+
+'""'
+      
+'""'
+print('He said, "John\'s program is easy to read"')
+      
+He said, "John's program is easy to read"
+
+print("He said, 'John\'s program is easy to read'")
+      
+He said, 'John's program is easy to read'
+
+print("c:\\desktop\\my_files\\1.py")
+      
+c:\desktop\my_files\1.py
+c:\desktop\my_files\1.py
+      
+SyntaxError: unexpected character after line continuation character
+
+
+
+"\n \t \" \' \\"
+      
+'\n \t " \' \\'
+
+
+print("\n \t \" \' \\")
+      
+
+ 	 " ' \
+
+print("c:\desktop\my_files\1.py")
+      
+c:\desktop\my_files.py
+
+
+
+print("Paata")
+      
+Paata
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+Paata
+50
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+Paata
+50
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+Paata
+50
+Paata
+50
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+Paata
+50
+Paata 50
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+Paata
+50
+Paata 50 
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+Paata
+50
+Paata 50 y = 53.95
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+Paata
+50
+Paata 50 y = 53.95
+Ok
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+AAA BBBCCC***DDD***
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+AAA BBBCCC***DDD***100
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+AAA BBBCCC***DDD***100
+x = 20
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+The area is 28.274333882308138 and the perimeter is 18.84955592153876
+
+============= RESTART: D:/199/7_6/22.11.14/print_functun.py ============
+The area is 28.27 and the perimeter is 18.85
+
+
+
+
+int('15')
+      
+15
+float('1.56')
+      
+1.56
+eval('12')
+      
+12
+eval('1.362')
+      
+1.362
+
+str(112)
+      
+'112'
+
+
+name = "Paata"
+      
+age = 50
+      
+print("My name is", name, "and i am", age, "years")
+      
+My name is Paata and i am 50 years
+
+
+2 + 6
+      
+8
+"paata" + "50"
+      
+'paata50'
+
+"2" + "6"
+      
+'26'
+
+
+>>> "paata" + " " + "50"
+...       
+'paata 50'
+>>> "paata " + "50"
+...       
+'paata 50'
+>>> 
+>>> 
+>>> name
+...       
+'Paata'
+>>> name + age
+...       
+Traceback (most recent call last):
+  File "<pyshell#57>", line 1, in <module>
+    name + age
+TypeError: can only concatenate str (not "int") to str
+>>> 
+>>> name + str(age)
+...       
+'Paata50'
+>>> 
+>>> 
+>>> print("My name is " + name + " and i am " + str(age) + " years.")
+...       
+My name is Paata and i am 50 years.
+>>> 
+>>> 
