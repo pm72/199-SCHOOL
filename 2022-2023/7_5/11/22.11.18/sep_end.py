9dde417cd5d36719e2feebf76d8d3bd9ce0bae99
pm72 <46081859+pm72@users.noreply.github.com>
2022-11-18 13:48:53 +0400

    pm72

---
+++ 2022-2023/7_5/11/22.11.18/sep_end.py
@@ -0,0 +1,14 @@
+r = 3
+
+print("The area of the circle is", 3.14 * r ** 2, end=' ')
+print("and its length is", 2 * 3.14 * r)
+print("Ok")
+print(14, 15, 9, sep='\n\n')
+
+
+print("AAA", end = ' ')         # 'AAA '
+print("BBB", end = '')          # 'AAA BBB'
+print("CCC", end = '***')       # 'AAA BBBCCC***'
+print("DDD", end = '***')       # 'AAA BBBCCC***DDD***'
+print("Hi")
+print("Hola")
