7a31e3ba004f80b515cfae4ec0f31f7bc881b7b5
pm72 <46081859+pm72@users.noreply.github.com>
2022-11-30 11:19:07 +0400

    pm72

---
+++ 2022-2023/7_6/2022/09/22.09.26/compute_area.py
@@ -0,0 +1,14 @@
+## წრის ფართობი:  s = pi * r * r, სადაც pi = 3.14159
+
+radius_of_circle = 20
+area_of_circle = 3.14159 * radius_of_circle ** 2
+
+print("The area of the circle is", area_of_circle)
+
+
+## ინფორმაცია
+## ტექსტი   string   პითონში str
+##
+## რიცხვითი:   მთელი int, ათწილადი float
+## int 5 8 9 0 -69 55 ...
+## float 1.89 6.0589 -5.2147  1.0
---
+++ 2022-2023/7_6/2022/11/22.11.14/print_functun.py
@@ -0,0 +1,27 @@
+##print("Paata")
+##print("50")
+##print("Paata", end=' ')     # paata 
+##print("50", end=' ')                 # paata 50
+##                            #
+##
+##x = 45
+##y = x + 8.95
+##
+##print("y =", y)
+##print("Ok")
+
+
+##print("AAA", end = ' ')       # "AAA "
+##print("BBB", end = '')        # "AAA BBB"
+##print("CCC", end = '***')     # "AAA BBBCCC***"
+##print("DDD", end = '***')     # "AAA BBBCCC***DDD***"
+##print(100)                    # "AAA BBBCCC***DDD***100"
+##print("x = 20")               # "x = 20"
+
+import math
+
+radius = 3
+print("The area is", round(radius * radius * math.pi, 2), end = ' ')
+print("and the perimeter is", round(2 * radius * math.pi, 2))
+
+
