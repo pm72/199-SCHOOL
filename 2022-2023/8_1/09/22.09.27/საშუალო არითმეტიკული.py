c37e05d9dde2b0385651bffac51ca8e56e90dfdd
pm72 <46081859+pm72@users.noreply.github.com>
2022-11-30 11:25:27 +0400

    pm72

--- 2022-2023/8_1/09/22.09.27/საშუალო არითმეტიკული.py
+++
@@ -1,9 +0,0 @@
-##numb1 = eval(input("Enter the first number: "))
-##numb2 = eval(input("Enter the second number: "))
-##numb3 = eval(input("Enter the third number: "))
-
-numb1, numb2, numb3 = eval(input("Enter three numbers: "))
-
-average = (numb1 + numb2 + numb3) / 3
-
-print("The average is", round(average, 2))
