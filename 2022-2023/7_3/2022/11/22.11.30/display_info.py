b2975a0e61395dada735fc9675c26784373b55b0
pm72 <46081859+pm72@users.noreply.github.com>
2022-11-30 14:40:15 +0400

    pm72

---
+++ 2022-2023/7_3/2022/11/22.11.30/display_info.py
@@ -0,0 +1,31 @@
+from chess_players import players
+
+# SPACES
+w1 = 3
+w2 = 20
+w3 = 2 * w1 + 3 * w2 + len('Age')
+ch = '-'
+
+sorted_players = sorted(players.items(),
+                        key=lambda i: i[1].get('Rating'),
+                        reverse=True)
+
+# HEAD
+head = f"\n{' ':<{w1}s}" \
+       f"{'Name':<{w2}s}" \
+       f"{'Country':<{w2}s}" \
+       f"{'Rating':<{w2}s}" \
+       f"{'Age'}\n" \
+       f"{ch * w3}"
+
+
+# DISPLAY INFO
+print(head)
+
+for name, info in sorted_players:
+  print(f"{' ':<{w1}s}" \
+       f"{name:<{w2}s}" \
+       f"{info.get('Country'):<{w2}s}" \
+       f"{info.get('Rating'):<{w2}d}" \
+       f"{info.get('Age')}\n" \
+       f"{ch * w3}")
