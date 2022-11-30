949ff0d2ef73764c0155b9f49dfdea9df2c9cf09
pm72 <46081859+pm72@users.noreply.github.com>
2022-11-30 11:04:16 +0400

    pm72

---
+++ 2022-2023/7_2/2022/09/22.09.20/input_method.py
@@ -0,0 +1,7 @@
+name = input("Name: ")
+age = int(input("Age: "))
+
+print("Hello, my name is", name)
+print("I am", age, "years old.")
+print(age, type(age))
+print("After 10 yers I will be", age + 10)
---
+++ 2022-2023/7_2/2022/11/22.11.17/characters.py
@@ -0,0 +1,10 @@
+import math as m
+
+r = 3
+
+print("Circles length is", round(2 * m.pi * r, 2), end=' ')
+print("and tha area is", round(m.pi * r ** 2, 2))
+print("Ok")
+
+
+print()
