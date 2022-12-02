5d193d92471eeb3d9b6c04b6d8beb59dd58320b4
pm72 <46081859+pm72@users.noreply.github.com>
2022-12-02 12:02:07 +0400

    pm72

---
+++ 2022-2023/7_1/2022/12/22.12.02/save_to_file.py
@@ -0,0 +1,32 @@
+from chess_players import players
+import csv, os
+
+sorted_players = sorted(players.items(),
+                        key=lambda i: (i[1].get('Country'),
+                                       i[1].get('Age'),
+                                       i[1].get('Rating')),
+                        reverse=True)
+
+FILENAME = "sorted players by countries&ages.csv"
+COLUMNS = ("Name", "Country", "Rating", "Age")
+
+with open(FILENAME, mode='w', newline='') as file:
+  w = csv.DictWriter(file, fieldnames=COLUMNS)
+
+  w.writeheader()
+
+  for name, info in sorted_players:
+    player = {
+      'Name': name,
+      'Country': info.get('Country'),
+      'Rating': info.get('Rating'),
+      'Age': info.get('Age'),
+      }
+
+    w.writerow(player)
+
+  
+
+os.startfile(FILENAME)
+
+  
