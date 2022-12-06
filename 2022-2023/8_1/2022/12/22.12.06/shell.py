cd33330e5dede40573d2e5231bc18d00377fbef3
pm72 <46081859+pm72@users.noreply.github.com>
2022-12-06 12:06:05 +0400

    pm72

---
+++ 2022-2023/8_1/2022/12/22.12.06/shell.py
@@ -0,0 +1,62 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(f"{10.5894566:.2f}")
+10.59
+
+
+import math as m
+m.sin(m.pi / 5)
+0.5877852522924731
+
+
+x1 = 39.55
+x1 = m.radians(x1)
+x1
+0.6902777191637572
+
+m.degrees(x1)
+39.55
+39.55
+39.55
+
+
+# arccos()   =====> m.acos()   arcsin()  ====> m.asin()
+
+
+# a = m.sqrt((x1 - x2) ** 2 + (y1 - y2)** 2)   5-jer
+
+# area1 = m.sqrt(p * (p - a) * (p - a) * (p - a))
+# p = (a + b + c) / 2
+
+
+
+# chr()   ord()
+chr(97)
+'a'
+>>> chr(65)
+'A'
+>>> 
+>>> ord('a')
+97
+>>> ord('a') - ord('A')
+32
+>>> 
+>>> 
+>>> 
+>>> from time import time
+>>> time()
+1670313314.677477
+>>> time()
+1670313322.5882
+>>> 
+>>> int(time())
+1670313331
+>>> 
+>>> 
+>>> 
+>>> # print("Deductions:")
+>>> # print("\tFederal Withholding (20.0%): $19.5")
+>>> 
+>>> 
+>>> 
+>>> x = 1005
