7098cc00ddb90be5da43cedbc9c3d0ef7e18556e
pm72 <46081859+pm72@users.noreply.github.com>
2022-12-09 12:56:30 +0400

    pm72

---
+++ 2022-2023/8_7/2022/12/22.12.09/shell.py
@@ -0,0 +1,102 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import math as m
+m.pi
+3.141592653589793
+m.sin(m.pi / 2)
+1.0
+m.sin(m.pi / 5)
+0.5877852522924731
+
+r = eval(input("Enter the length from the center to a vertex: "))
+Enter the length from the center to a vertex: 5.5
+
+print(f"The area of the pentagon is 108.6142589")
+The area of the pentagon is 108.6142589
+
+area = 108.6142589
+print(f"The area of the pentagon is area")
+The area of the pentagon is area
+print(f"The area of the pentagon is {area}")
+The area of the pentagon is 108.6142589
+print(f"The area of the pentagon is {area:.2}")
+The area of the pentagon is 1.1e+02
+print(f"The area of the pentagon is {area:.2f}")
+The area of the pentagon is 108.61
+
+
+x1, y1 = 39.55, -116.25
+x1 = m.radians(x1)
+y1 = m.radians(y1)
+x1, y1
+(0.6902777191637572, -2.028945255443408)
+m.sin(x1), m.sin(y1)
+(0.636751347470152, -0.8968727415326884)
+
+m.degrees(x1), m.degrees(y1)
+(39.55, -116.25)
+
+# arccos()  ====> m.acos()   arcsin()   ====> m.asin()  ...
+
+
+
+# a = m.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+# b = ... c = ... d = ...  e = ...
+# p1 = (a + b + e) / 2
+# p2 = ...
+
+# S1 = m.sqrt(p * (p - a) * (p - b) * (p - e))
+# S2 = ...
+# S = S1 + S2
+
+
+
+# tan()   ====> m.tan()
+
+
+# chr()    ord()
+chr(97)
+'a'
+chr(69)
+'E'
+chr(65)
+'A'
+
+ord('a')
+97
+ord('A')
+65
+ord('E')
+69
+ord('a') - ord('A')
+32
+ord('r') - ord('R')
+32
+>>> 
+>>> 
+>>> 
+>>> from time import time
+>>> time()
+1670575677.8656685
+>>> int(time())
+1670575696
+>>> int(time())
+1670575698
+>>> int(time())
+1670575700
+>>> int(time())
+1670575702
+>>> 1670575702
+1670575702
+
+>>> 
+>>> # [65 .. 90]
+>>> 
+>>> 
+>>> 
+>>> # print("Deductions:")
+>>> # print("\tFederal Withholding (20.0%): $19.5")
+>>> 
+>>> 
+>>> 
+>>> x = 1005
