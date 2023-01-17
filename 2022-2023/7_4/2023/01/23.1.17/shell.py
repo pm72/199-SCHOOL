562bc83258640208026aeb287232fee894d1416e
pm72 <46081859+pm72@users.noreply.github.com>
2023-01-17 14:36:03 +0400

    pm72

---
+++ 2022-2023/7_4/2023/01/23.1.17/shell.py
@@ -0,0 +1,104 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ==============
+Enter a radius: -8
+Wrong input, enter positive number...
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 2.56
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 2.57
+The area is 20.75
+
+
+  a = 9
+  
+SyntaxError: unexpected indent
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 2.58
+The area is 20.91
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 5
+The area is 78.54
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -5
+Wrong input, enter positive number...
+Traceback (most recent call last):
+  File "D:/199/7_4/2023/01/23.1.17/circle_area.py", line 9, in <module>
+    print(f"The area is {area:.2f}")
+NameError: name 'area' is not defined
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -8
+Wrong input, enter positive number...
+The area is 0.00
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 2.89
+The area is 26.24
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -2
+The area is 0.00
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 0
+The area is 0.00
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 5.47
+The area is 94.00
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -2.58
+The area is 0.00
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -2
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -8
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 2.58
+The area is 20.91
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -4
+Traceback (most recent call last):
+  File "D:/199/7_4/2023/01/23.1.17/circle_area.py", line 18, in <module>
+    d = area + radius
+NameError: name 'area' is not defined
+
+
+
+
+
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -8
+The area is 0.00
+d = 0.0
+>>> 
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -8
+d = 0.0
+>>> 
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -5
+d = 1.5811388300841898
+>>> 
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: -4
+Traceback (most recent call last):
+  File "D:/199/7_4/2023/01/23.1.17/circle_area.py", line 19, in <module>
+    print(f"d = {m.sqrt(area)}")
+ValueError: math domain error
+>>> 
+==== RESTART: D:/199/7_4/2023/01/23.1.17/circle_area.py ====
+Enter a radius: 2.17
+The area is 14.79
