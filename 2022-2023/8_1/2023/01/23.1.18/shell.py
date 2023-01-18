ba9a345cf3cf57c148d3f8e038bfe7ba9488040d
pm72 <46081859+pm72@users.noreply.github.com>
2023-01-18 13:50:16 +0400

    pm72

---
+++ 2022-2023/8_1/2023/01/23.1.18/shell.py
@@ -0,0 +1,89 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py ==============
+Enter a radius: 2.56
+The area is 20.59
+
+Done!
+
+
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: -8
+Wrong input. Enter a positive number.
+
+Done!
+
+
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: 2.36
+The area is 17.50
+
+Done!
+
+
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: 0
+The area is 0.00
+
+Done!
+
+
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: -5
+Wrong input. Enter a positive number.
+Traceback (most recent call last):
+  File "D:\199\8_1\2023\01\23.1.18\circle_area.py", line 23, in <module>
+    print(f"The area is {area:.2f}")
+NameError: name 'area' is not defined
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: 2.58
+The area is 20.91
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: 0
+The area is 0.00
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: -8
+The area is 0.00
+
+= RESTART: D:\199\8_1\2023\01\23.1.18\circle_area.py
+Enter a radius: -5
+The area is 0.00
+d = 0.0
+
+Done!
+
+
+
+
+
+# >, >=, <, <=, ==, !=
+
+a = 5
+a
+5
+>>> a == 5
+True
+>>> a != 5
+False
+>>> 
+>>> a > 5
+False
+>>> 
+>>> a >= 5
+True
+>>> 
+>>> a < 5
+False
+>>> 
+>>> a <= 5
+True
+>>> 
+>>> # int, float, str
