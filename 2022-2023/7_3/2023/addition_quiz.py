09c3f506d4f92dc90d21a630cecc9f6b67c5d39e
pm72 <46081859+pm72@users.noreply.github.com>
2023-01-27 12:57:06 +0400

    pm72

---
+++ 2022-2023/7_3/2023/addition_quiz.py
@@ -0,0 +1,20 @@
+import random as r
+
+x = r.randint(0, 9)
+y = r.randint(0, 9)
+
+answer = int(input(f"What is {x} + {y}? "))
+
+print(f"{x} + {y} = {answer} is {x + y == answer}")
+
+if x + y != answer:
+  print(f"{x} + {y} is {x + y}")
+
+
+"""
+  r.randint(a, b)
+  r.randrange(a, b-1)    r.randrange(0, 9)
+
+  r.random()    range[0.0, 1.0)
+  r.uniform(a, b)
+"""
