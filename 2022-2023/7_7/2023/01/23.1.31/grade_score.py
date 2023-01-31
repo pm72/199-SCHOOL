5a6c43ecec98690f0a16d8d3c6445b3829ecf683
pm72 <46081859+pm72@users.noreply.github.com>
2023-01-31 10:59:30 +0400

    pm72

---
+++ 2022-2023/7_7/2023/01/23.1.31/grade_score.py
@@ -0,0 +1,17 @@
+score = int(input("Enter a score: "))
+
+if score >= 90:
+  grade = 'A'
+else:
+  if score >= 80:
+    grade = 'B'
+  else:
+    if score >= 70:
+      grade = 'C'
+    else:
+      if score >= 60:
+        grade = 'D'
+      else:
+        grade = 'F'
+
+print(f"Your grade is {grade}")
