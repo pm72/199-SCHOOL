4f63e0def060bbeb33cc3d0015092c783709d696
pm72 <46081859+pm72@users.noreply.github.com>
2023-01-31 11:39:19 +0400

    pm72

--- 2022-2023/7_1/2023/compute_tax.py
+++ 2022-2023/7_1/2023/compute_tax.py
@@ -4,7 +4,7 @@ status = int(input(
   "0 – Single filer\n" + \
   "1 – Married jointly\n" + \
   "2 – Married separately\n" + \
-  "3 – Head of household\n" + \
+  "3 – Head of household\n\n" + \
   "Enter the filing status: "))
 
 income = eval(input("Enter the taxable income: "))
@@ -17,17 +17,19 @@ if status == 0:
   elif income <= 33950:
     tax = 8350 * 0.1 + (income - 8350) * 0.15
   elif income <= 82250:
-    tax = 8350 * 0.1 + 33950 * 0.15 + (income - 33950) * 0.25
+    tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + \
+          (income - 33950) * 0.25
   elif income <= 171550:
-    tax = 8350 * 0.1 + 33950 * 0.15 + 33950 * 0.25 + \
-          (income - 82250) * 0.28
+    tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + \
+          (82250 - 33950) * 0.25 + (income - 82250) * 0.28
   elif income <= 372950:
-    tax = 8350 * 0.1 + 33950 * 0.15 + 33950 * 0.25 + \
-          82250 * 0.28 + (income - 171550) * 0.33
+    tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + \
+          (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + \
+          (income - 171550) * 0.33
   else:
-    tax = 8350 * 0.1 + 33950 * 0.15 + 33950 * 0.25 + \
-          82250 * 0.28 + 171550 * 0.33 + \
-          (income - 372950) * 0.35
+    tax = 8350 * 0.1 + (33950 - 8350) * 0.15 + \
+          (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + \
+          (372950 - 171550) * 0.33 + (income - 372950) * 0.35
 elif status == 1:
   ...
 elif status == 2:
@@ -38,4 +40,5 @@ else:
   print("Error: invalid status")
   sys.exit()
 
-print(f"Tax is {tax:.2f}")
+print(f"\nTax is {tax:.2f}")
+print(f"Total income is {income - tax:.2f}")
