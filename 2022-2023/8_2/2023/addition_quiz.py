1df713e4b70359bd6cc0894fac40d47673f9aec6
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-02 14:34:43 +0400

    pm72

---
+++ 2022-2023/8_2/2023/addition_quiz.py
@@ -0,0 +1,11 @@
+import random as r
+
+x = r.randint(0, 10)
+y = r.randint(0, 10)
+
+answer = int(input(f"What is {x} + {y}? "))
+
+print(f"{x} + {y} = {answer} is {x + y == answer}")
+
+if x + y != answer:
+  print(f"{x} + {y} = {x+ y}")
---
+++ 2022-2023/8_2/2023/grade.py
@@ -0,0 +1,41 @@
+score = -1
+
+# while score < 0 or score > 100
+while not (0 <= score <= 100):
+  score = int(input("Enter score [max = 100, min = 0]: "))
+
+##if score > 90:
+##  grade = 10
+##else:
+##  if score > 80:
+##    grade = 9
+##  else:
+##    if score > 70:
+##      grade = 8
+##    else:
+##      if score > 60:
+##        grade = 7
+##      else:
+##        if score > 50:
+##          grade = 6
+##        else:
+##          grade = 5
+
+
+if score > 90:
+  grade = 10
+elif score > 80:
+    grade = 9
+elif score > 70:
+  grade = 8
+elif score > 60:
+  grade = 7
+elif score > 50:
+  grade = 6
+else:
+  grade = 5
+
+
+print(f"\nResult:\n" + \
+      f"Score: {score}\n" + \
+      f"Grade: {grade}")
