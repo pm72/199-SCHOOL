e1fa5c0e6b76cdb8b4fe445edb62e4f22557ff08
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-08 12:58:02 +0400

    pm72

--- 2022-2023/7_3/2023/compute_tax_usa.py
+++ 2022-2023/7_3/2023/compute_tax_usa.py
@@ -1,4 +1,8 @@
 import sys
+import single_filer as sf
+import married_jointly as mj
+import married_separately as ms
+import head_of_household as hoh
 
 status = int(input("0 – Single filer\n" + \
                    "1 – Married jointly\n" + \
@@ -12,41 +16,19 @@ if status < 0 or status > 3:
   sys.exit()
 
 income = eval(input("Enter the taxable income: "))
+tax = 0
 
 if status == 0:
-  fix10 = 8350
-  fix15 = 33950
-  fix25 = 82250
-  fix28 = 171550
-  fix33 = 372950
-
-  if income <= fix10:
-    tax = income * 0.1
-  elif income <= fix15:
-    tax = fix10 * 0.1 + (income - fix10) * 0.15
-  elif income <= fix25:
-    tax = fix10 * 0.1 + (fix15 - fix10)* 0.15 + \
-          (income - fix15) * 0.25
-  elif income <= fix28:
-    tax = fix10 * 0.1 + (fix15 - fix10)* 0.15 + \
-          (fix25 - fix15) * 0.25 + (income - fix25) * 0.28
-  elif income <= fix33:
-    tax = fix10 * 0.1 + (fix15 - fix10)* 0.15 + \
-          (fix25 - fix15) * 0.25 + (fix28 - fix25) * 0.28 + \
-          (income - fix28) * 0.33
-  else:
-    tax = fix10 * 0.1 + (fix15 - fix10)* 0.15 + \
-          (fix25 - fix15) * 0.25 + (fix28 - fix25) * 0.28 + \
-          (fix33 - fix28) * 0.33 + (income - fix33) * 0.35  
+  tax = sf.tax(income)
 elif status == 1: 
-  print("\nMarried jointlyr")
+  tax = mj.tax(income)
 elif status == 2:
-  ...
+  tax = ms.tax(income)
 else:
-  ...
+  tax = hoh.tax(income)
 
 
-print(f"Tax is {tax:.2f}\n" + \
+print(f"\nTax is {tax:.2f}\n" + \
       f"Total income: {income - tax:.2f}")
 
 input()
