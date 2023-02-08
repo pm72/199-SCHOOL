22d90096cee136b60ecfe1c2327f6949311fc3c7
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-08 14:36:39 +0400

    pm72

---
+++ 2022-2023/8_6/2023/compute_tax_USA.py
@@ -0,0 +1,34 @@
+import sys
+import single_filer as sf
+import married_jointly as mj
+import married_separately as ms
+import head_of_household as hoh
+
+status = int(input("0 – single filer\n" + \
+                   "1 – Married jointly\n" + \
+                   "2 – Married separately\n" + \
+                   "3 – head of household\n\n" + \
+                   "Enter the filing status: "))
+
+if status < 0 or status > 3:
+  print("Error: invalid status")
+  input()
+  sys.exit()
+
+income = eval(input("Enter income: "))
+
+if status == 0:
+  tax = sf.tax(income)
+elif status == 1:   # else if == 1:
+  tax = mj.tax(income)
+elif status == 2:
+  tax = ms.tax(income)
+else:
+  tax = hoh.tax(income)
+
+print(f"\nTax: {tax:.2f}\n" + \
+      f"Remamaining income: {income - tax:.2f}")
+
+
+# ===============
+input()
