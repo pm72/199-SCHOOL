290aca12cbba4cf12e39bca8f721af6fc283b547
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-10 09:25:15 +0400

    pm72

---
+++ 2022-2023/7_4/2023/compute_tax_USA.py
@@ -0,0 +1,55 @@
+import sys
+
+status = int(input("0 – Single filer\n" + \
+                   "1 – Married jointly\n" + \
+                   "2 – Married separately\n" + \
+                   "3 – Head of household\n\n" + \
+                   "Enter the filing status: "))
+
+if status < 0 or status > 3:
+  print("Error: invalid status")
+  input()
+  sys.exit()
+
+income = eval(input("Enter income: "))
+
+if status == 0:
+  fix10 = 8350
+  fix15 = 33950
+  fix25 = 82250
+  fix28 = 171550
+  fix33 = 372950
+  
+  if income <= fix10:
+    tax = income * 0.1
+  elif income <= fix15:
+    tax = fix10 * 0.1 + (income - fix10) * 0.15
+  elif income <= fix25:
+    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
+          (income - fix15) * 0.25
+  elif income <= fix28:
+    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
+          (fix25 - fix15) * 0.25 + (income - fix25) * 0.28
+  elif income <= fix33:
+    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
+          (fix25 - fix15) * 0.25 + (fix28 - fix25) * 0.28 + \
+          (income - fix28) * 0.33
+  else:
+    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
+          (fix25 - fix15) * 0.25 + (fix28 - fix25) * 0.28 + \
+          (fix33 - fix28) * 0.33 + (income - fix33) * 0.35
+elif status == 1:
+  pass
+elif status == 2:
+  pass
+else:
+  pass
+
+
+print(f"\nTax: {tax:.2f}\n" + \
+      f"Remainng income: {(income - tax):.2f}")
+
+
+
+# =============
+input()
