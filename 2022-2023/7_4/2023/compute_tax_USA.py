5e67259759323e35a2d82d082eec47c2a21de549
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-14 14:39:20 +0400

    pm72

--- 2022-2023/7_4/2023/compute_tax_USA.py
+++ 2022-2023/7_4/2023/compute_tax_USA.py
@@ -1,49 +1,47 @@
 import sys
 
-status = int(input("0 – Single filer\n" + \
-                   "1 – Married jointly\n" + \
-                   "2 – Married separately\n" + \
-                   "3 – Head of household\n\n" + \
-                   "Enter the filing status: "))
-
-if status < 0 or status > 3:
-  print("Error: invalid status")
+try:
+  import single_filer as sf
+  import married_jointly as mj
+  import married_separately as ms
+  import head_of_household as hoh
+except ModuleNotFoundError as error:
+  print(error)
   input()
   sys.exit()
 
-income = eval(input("Enter income: "))
+status = 0.0
+while type(status) == float:
+  try:
+    status = int(input("0 – Single filer\n" + \
+                       "1 – Married jointly\n" + \
+                       "2 – Married separately\n" + \
+                       "3 – Head of household\n\n" + \
+                       "Enter the filing status: "))
+    if status < 0 or status > 3:
+      print("\nError: invalid status\n")
+      status = 0.0
+      continue
+  except ValueError:
+    print("\nPlease, enter an integer between 0 and 3...\n")
+
+income = ''
+while type(income) != int and type(income) != float:
+  try:
+    income = eval(input("Enter income: "))
+  except SyntaxError:
+    print("\nPlease enter number for income...\n")
+  except NameError:
+    print("\nPlease enter number for income...\n")
 
 if status == 0:
-  fix10 = 8350
-  fix15 = 33950
-  fix25 = 82250
-  fix28 = 171550
-  fix33 = 372950
-  
-  if income <= fix10:
-    tax = income * 0.1
-  elif income <= fix15:
-    tax = fix10 * 0.1 + (income - fix10) * 0.15
-  elif income <= fix25:
-    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
-          (income - fix15) * 0.25
-  elif income <= fix28:
-    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
-          (fix25 - fix15) * 0.25 + (income - fix25) * 0.28
-  elif income <= fix33:
-    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
-          (fix25 - fix15) * 0.25 + (fix28 - fix25) * 0.28 + \
-          (income - fix28) * 0.33
-  else:
-    tax = fix10 * 0.1 + (fix15 - fix10) * 0.15 + \
-          (fix25 - fix15) * 0.25 + (fix28 - fix25) * 0.28 + \
-          (fix33 - fix28) * 0.33 + (income - fix33) * 0.35
+  tax = sf.tax(income)
 elif status == 1:
-  pass
+  tax = mj.tax(income)
 elif status == 2:
-  pass
+  tax = ms.tax(income)
 else:
-  pass
+  tax = hoh.tax(income)
 
 
 print(f"\nTax: {tax:.2f}\n" + \
