6c20565d270c02036c5035ba6bf683b2da89e20d
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-14 10:10:24 +0400

    pm72

---
+++ 2022-2023/8_5/2023/substracy_quiz.py
@@ -0,0 +1,15 @@
+import random as r
+
+x = r.randint(0, 10)
+y = r.randint(0, 10)
+
+if x < y:
+  x, y = y, x
+
+answer = int(input(f"What is {x} - {y}? "))
+
+print(f"{x} - {y} = {answer} is {x - y == answer}")
+
+
+if x + y != answer:
+  print(f"{x} - {y} = {x - y}")
