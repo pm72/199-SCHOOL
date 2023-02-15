87c149b95ce6c7d6a7fb619829bf0d9948de059a
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-15 12:58:35 +0400

    pm72

--- 2022-2023/7_3/2023/compute_tax_usa 2.py
+++ 2022-2023/7_3/2023/compute_tax_usa 2.py
@@ -1,8 +1,14 @@
 import sys
-import single_filer as sf
-import married_jointly as mj
-import married_separately as ms
-import head_of_household as hoh
+
+try:
+  import single_filer as sf
+  import married_jointly as mj
+  import married_separately as ms
+  import head_of_household as hoh
+except ModuleNotFoundError as error:
+  print(error)
+  input("\nPlease, type 'ENTER' to exit...")
+  sys.exit()
 
 status = 0.0
 while type(status) == float:
@@ -14,26 +20,35 @@ while type(status) == float:
                        "Enter the filing status: "))
   except ValueError:
     print("\nPlease, enter an integer...\n")
+  
+  if status < 0 or status > 3:
+    status = 0.0
+    print("\nError: invalid status\n ")
 
-if status < 0 or status > 3:
-  print("\nError: invalid status")
-  input()
-  sys.exit()
-
-income = eval(input("Enter the taxable income: "))
-tax = 0
+income = ''
+while type(income) != int and type(income) != float:
+  try:
+    income = eval(input("Enter the taxable income: "))
+  except NameError:
+    print("\nPlease, enter an integer for 'income'...\n")
+  except SyntaxError:
+    print("\nPlease, enter an integer for 'income'...\n")
 
 if status == 0:
   tax = sf.tax(income)
+  status = "Single filer"
 elif status == 1: 
   tax = mj.tax(income)
+  status = "Married jointly"
 elif status == 2:
   tax = ms.tax(income)
+  status = "Married separately"
 else:
   tax = hoh.tax(income)
+  status = "Head of household"
 
-
-print(f"\nTax is {tax:.2f}\n" + \
+print(f"\nStatus: {status}\n" + \
+      f"Tax is {tax:.2f}\n" + \
       f"Total income: {income - tax:.2f}")
 
 input()
