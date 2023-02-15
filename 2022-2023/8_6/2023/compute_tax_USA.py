8c8aac7c1ad8a179fb04d1aa187628c71ec9c3b5
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-15 14:44:48 +0400

    pm72

--- 2022-2023/8_6/2023/compute_tax_USA.py
+++ 2022-2023/8_6/2023/compute_tax_USA.py
@@ -1,32 +1,55 @@
 import sys
-import single_filer as sf
-import married_jointly as mj
-import married_separately as ms
-import head_of_household as hoh
 
-status = int(input("0 – single filer\n" + \
-                   "1 – Married jointly\n" + \
-                   "2 – Married separately\n" + \
-                   "3 – head of household\n\n" + \
-                   "Enter the filing status: "))
-
-if status < 0 or status > 3:
-  print("Error: invalid status")
-  input()
+try:
+  import single_filer as sf
+  import married_jointly as mj
+  import married_separately as ms
+  import head_of_household as hoh
+except ModuleNotFoundError as error:
+  print(error)
+  input("\nPlease, type 'ENTER' to exit...")
   sys.exit()
 
-income = eval(input("Enter income: "))
+status = ''   # str
+while type(status) != int:
+  try:
+    status = int(input("0 – Single filer\n" + \
+                       "1 – Married jointly\n" + \
+                       "2 – Married separately\n" + \
+                       "3 – Head of household\n\n" + \
+                       "Enter the filing status: "))
+  except ValueError:
+    print("\nPlease, enter an integer...\n")
+    continue
 
+  if status < 0 or status > 3:
+    print("\nError: invalid status\n")
+    status = ''
+
+income = ''
+while type(income) != int and type(income) != float:
+  try:
+    income = eval(input("Enter income: "))
+  except SyntaxError:
+    print("\nPlese, enter number for 'Income'...\n")
+  except NameError:
+    print("\nPlese, enter number for 'Income'...\n")
+  
 if status == 0:
   tax = sf.tax(income)
+  status = "Single filer"
 elif status == 1:   # else if == 1:
   tax = mj.tax(income)
+  status = "Married jointly"
 elif status == 2:
   tax = ms.tax(income)
+  status = "Married separately"
 else:
   tax = hoh.tax(income)
+  status = "Head of household"
 
-print(f"\nTax: {tax:.2f}\n" + \
+print(f"\nStatus: {status}\n" + \
+      f"Tax: {tax:.2f}\n" + \
       f"Remamaining income: {income - tax:.2f}")
 
 
