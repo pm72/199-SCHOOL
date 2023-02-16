f512d276ee5899ea0489f2fdda925774f6918442
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-16 10:57:00 +0400

    pm72

---
+++ 2022-2023/7_6/2023/02/23.02.02/grade.py
@@ -0,0 +1,37 @@
+score = int(input("Enter score [max = 100, min = 0]: "))
+
+##if score > 90:
+##  grade = 10
+##else:
+##  if score > 80:
+##    grade = 9
+##  else:
+##    if score > 70:
+##      grade = 8
+##    else:
+##      if score > 60:
+##        grade = 7
+##      else:
+##        if score > 50:
+##          grade = 6
+##        else:
+##          grade = 5
+
+
+if score > 90:
+  grade = 10
+elif score > 80:
+  grade = 9
+elif score > 70:
+  grade = 8
+elif score > 60:
+  grade = 7
+elif score > 50:
+  grade = 6
+else:
+  grade = 5
+
+
+print(f"\nResult:\n" + \
+      f"Score: {score}\n" + \
+      f"Grade: {grade}")
