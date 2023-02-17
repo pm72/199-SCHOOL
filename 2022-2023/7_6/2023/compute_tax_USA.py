29ca9e97176b58d86659060186c9a7e4b5f63200
pm72 <46081859+pm72@users.noreply.github.com>
2023-02-17 11:08:03 +0400

    pm72

--- 2022-2023/7_6/2023/compute_tax_USA.py
+++ 2022-2023/7_6/2023/compute_tax_USA.py
@@ -10,18 +10,30 @@ except ModuleNotFoundError as error:
   input("\nPress 'Enter' to exit...")
   sys.exit()
 
-status = int(input("0 – Single filer\n" + \
-                   "1 – Married jointly\n" + \
-                   "2 – Married separately\n" + \
-                   "3 – Head of household\n\n" + \
-                   "Enter the filing status: "))
+status = ''
+while type(status) != int:
+  try:
+    status = int(input("0 – Single filer\n" + \
+                       "1 – Married jointly\n" + \
+                       "2 – Married separately\n" + \
+                       "3 – Head of household\n\n" + \
+                       "Enter the filing status: "))
+  except ValueError:
+    print("\nPlease, enter an integer for status...\n")
+    continue
 
-if status < 0 or status > 3:
-  print("Error: invalid status")
-  input()
-  sys.exit()
+  if status < 0 or status > 3:
+    print("\nError: invalid status\n")
+    status = ''
 
-income = eval(input("Enter income: "))
+income = ''
+while type(income) != int and type(income) != float:
+  try:
+    income = eval(input("Enter income: "))
+  except SyntaxError:
+    print("\nPlease, enter correct number for income...\n")
+  except NameError:
+    print("\nPlease, enter correct number for income...\n")
 
 if status == 0:
   tax = sf.tax(income)
