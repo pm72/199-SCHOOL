2dea1897fc0c2a78cc501e46bdeef1f7f5d04e48
pm72 <46081859+pm72@users.noreply.github.com>
2023-03-16 09:17:29 +0400

    pm72

---
+++ 2022-2023/7_7/2023/leap_year.py
@@ -0,0 +1,17 @@
+# int, float, str, None
+
+# bool
+
+
+year = int(input("Enter an year: "))
+
+leap_year = year % 4 == 0 and year % 100 != 0 or \
+            year % 400 == 0
+
+if leap_year:
+  print(f"{year} is leap")
+else:
+  print(f"{year} isn't leap")
+
+
+print(f"{year} is leap" if leap_year else f"{year} isn't leap")
