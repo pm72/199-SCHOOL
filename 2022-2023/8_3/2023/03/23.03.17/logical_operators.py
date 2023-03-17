5e65788f52ef205989e8ecd317484d9bc61871a4
pm72 <46081859+pm72@users.noreply.github.com>
2023-03-17 14:35:36 +0400

    pm72

---
+++ 2022-2023/8_3/2023/03/23.03.17/logical_operators.py
@@ -0,0 +1,26 @@
+'''
+int, float, str, None
+
+bool    =====>  True / False
+
+
+=====> NOT <=====
+not True = False
+not False = True
+
+not 5 = False
+not -1 = False
+not 0 = True
+not None = True
+not '' = True
+
+
+=====> AND <=====
+
+
+
+=====> OR <=====
+
+
+
+'''
