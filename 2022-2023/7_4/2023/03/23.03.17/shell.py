1451ce65f18748ed86c7c171205c20b3fd06b502
pm72 <46081859+pm72@users.noreply.github.com>
2023-03-17 09:19:47 +0400

    pm72

---
+++ 2022-2023/7_4/2023/03/23.03.17/shell.py
@@ -0,0 +1,64 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+not True
+False
+not False
+True
+not 5
+False
+not - 1
+False
+not 0
+True
+not 0.0
+True
+not ''
+True
+not None
+True
+
+
+5 > 3
+True
+
+not (5 > 3)
+False
+
+not 5 > 3
+False
+
+not 5 < 3
+True
+
+
+
+5 > 3 and not (2.78 <= -1.56)
+True
+
+x = 1900
+>>> x % 4 == 0 and x % 100 == 0
+True
+>>> 
+>>> x = 1902
+>>> x % 4 == 0 and x % 100 == 0
+False
+>>> 
+>>> 
+>>> 
+========= RESTART: D:/199/7_4/2023/leap_year.py =========
+Enter year: 2012
+2012 is leap
+>>> 
+========= RESTART: D:/199/7_4/2023/leap_year.py =========
+Enter year: 1800
+1800 isn't leap
+>>> 
+========= RESTART: D:/199/7_4/2023/leap_year.py =========
+Enter year: 1900
+1900 isn't leap
+1900 isn't leap
+>>> 
+========= RESTART: D:/199/7_4/2023/leap_year.py =========
+Enter year: 2000
+2000 is leap
+2000 is leap
