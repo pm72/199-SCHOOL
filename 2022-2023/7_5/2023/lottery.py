e4ffb23eb0d0760fa5d1e7ef0b3ea13dc71440d6
pm72 <46081859+pm72@users.noreply.github.com>
2023-03-23 13:47:17 +0400

    pm72

--- 2022-2023/7_5/2023/lottery.py
+++ 2022-2023/7_5/2023/lottery.py
@@ -1,26 +1,50 @@
 import random as r
+import sys
 
-lottery = r.randint(0, 99)
+balance = 0
+while balance < 100:
+  balance = eval(input("Fill your balance [min $100]: "))
 
-guess = int(input("Enter your lottery pick (two digits): "))
+while balance >= 100:
+  lottery = r.randint(0, 99)
 
-lottery_digit1 = lottery // 10
-lottery_digit2 = lottery % 10
+  guess = 0
+  while guess < 10 or guess > 99:
+    guess = int(input("Enter your lottery pick (two digits)" + \
+                      "(0 - Exit): "))
 
-guess_digit1 = guess // 10
-guess_digit2 = guess % 10
+    if guess == 0:
+      print(f"\nBalance: {balance}\n\n")
+      input("Game over...")
+      sys.exit()
 
-print("The lottery number is", lottery)
+  lottery_digit1 = lottery // 10
+  lottery_digit2 = lottery % 10
 
-if guess == lottery:
-  print("Exact match: you win $10,000\n")
-elif guess_digit1 == lottery_digit2 and \
-     guess_digit2 == lottery_digit1:
-  print("Match all digits: you win $3,000\n")
-elif guess_digit1 == lottery_digit1 or \
-     guess_digit1 == lottery_digit2 or \
-     guess_digit2 == lottery_digit1 or \
-     guess_digit2 == lottery_digit2:
-  print("Match one digit: you win $1,000")
-else:
-  print("Sorry, no match")
+  guess_digit1 = guess // 10
+  guess_digit2 = guess % 10
+
+  balance -= 100
+
+  if guess == lottery:
+    print("Exact match: you win $10000\n")
+    balance += 10000
+  elif guess_digit1 == lottery_digit2 and \
+       guess_digit2 == lottery_digit1:
+    print("Match all digits: you win $3000\n")
+    balance += 3000
+  elif guess_digit1 == lottery_digit1 or \
+       guess_digit1 == lottery_digit2 or \
+       guess_digit2 == lottery_digit1 or \
+       guess_digit2 == lottery_digit2:
+    print("Match one digit: you win $500")
+    balance += 500
+  else:
+    print("Sorry, no match")
+
+  print(f"\nLottery number was {lottery}")
+
+  print(f"\nBalance: {balance}\n\n")
+
+
+input("Game over...")
