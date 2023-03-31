22674c31cb2602c49bd43bbab56273fdad5298be
pm72 <46081859+pm72@users.noreply.github.com>
2023-03-31 12:57:13 +0400

    pm72

--- 2022-2023/7_6/2023/CH 5 (Loops)/5_0 while.py
+++ 2022-2023/7_6/2023/CH 5 (Loops)/5_0 while.py
@@ -1,65 +1,39 @@
 ##print("Programming is fun!\n" * 10)
 ##
-##text = "Programming is fun!\n" * 10
-##
-##print(text)
-
-
-'''
-while condition:
-  ...
-
-print("Programming is fun!")
-
-[start, stop, step]   [1, 10, 1]
-
-'''
-
 ##i = 1
 ##while i <= 10:
 ##  print(f"{i}: Programming is fun!")
 ##
-##  i += 1   # i = i + 1
+##  i += 1  # i = i + 1
 ##
-##print(f"{i}: ")
+##print(i)
 
 
-##i = 1
-##s = 0            # 0  1  3  6   10  15  21  28  36  45
-##while i <= 10:   # 1  2  3  4   5   6   7   8   9   10
-##  s += i         # 1  3  6  10  15  21  28  36  45  55
-##
+# [1, 10, 1]  [start, stop, step]
+
+##i = 2
+##jami = 1
+##namravli = 1
+##while i <= 10:    # 1  2  3  4   5   6   7   8   9   10
+##  jami += i       # 1  3  6  10  15  21  28  36  45  55
+##  namravli *= i
+##  
 ##  i += 1
 ##
-##print(f"The sum is {s}")
-
-
-
-##i = 123
-##s = 0
-##
-##if i % 2 == 1:
-##  i += 1
-##
-##while i <= 324:
-##  s += i
-##
-##  i += 2
-##
-##print(f"The sum is {s}")
+##print(f"The sum is {jami}")
+##print(f"The multiply is {namravli}")
 
 
 i = 123
 s = 0
+d = 2
 
-while i % 17 != 0:
-  i += 1
+if i % d != 0:
+  i += (d - i % d)
 
 while i <= 324:
   s += i
-
   print(i, end='  ')
-  
-  i += 17
+  i += d
 
-print(f"\nThe sum is {s}")
+print(f"\n\nThe sum is {s}")
