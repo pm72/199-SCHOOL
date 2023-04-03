2286357a096b19701bfe7ae80771db402188d348
pm72 <46081859+pm72@users.noreply.github.com>
2023-04-03 08:41:12 +0400

    pm72

--- 2022-2023/8_4/2023/01/23.1.16/circle.py
+++
@@ -1,8 +0,0 @@
-import math as m
-
-radius = eval(input("Enter a radius: "))
-
-if radius < 0:
-  print("Error! Enter a positive number...")
-else:
-  print(f"The are is {(m.pi * radius ** 2):.2f}")
