32ae9deffa6572931241ff49e201cd30d719d8a4
pm72 <46081859+pm72@users.noreply.github.com>
2023-04-03 09:11:20 +0400

    pm72

---
+++ 2022-2023/8_4/2023/CH 5 (Loops)/5_0 While.py
@@ -0,0 +1,40 @@
+'''
+
+while condition:    x < 3:
+  ...
+  ...
+  ...
+
+
+  [start, stop, step]    [1, 10, 1]
+
+'''
+
+##print("Programming is fun!..\n" * 10)
+##
+##text = "Programming is fun!..\n" * 10
+##
+##print(text)
+
+##i = 1
+##while i <= 10:
+##  print(f"{i}: Programming is fun!..")
+##
+##  i += 1   # i = i + 1
+##
+##print(f"{i}: ")
+
+
+
+i = 123
+s = 0
+d = 47
+
+i += (d - i % d)
+
+while i <= 324:
+  s += i
+  print(i, end='  ')
+  i += d
+
+print("\n\n",s)
