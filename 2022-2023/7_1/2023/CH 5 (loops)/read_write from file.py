95add693dee12045e66191f9cfd6d3f5a0a68f2b
pm72 <46081859+pm72@users.noreply.github.com>
2023-04-10 10:04:19 +0400

    pm72

---
+++ 2022-2023/7_1/2023/CH 5 (loops)/read_write from file.py
@@ -0,0 +1,23 @@
+from sys import argv
+
+with open(file="input.txt", mode='r', encoding='utf8') as fin:
+  data = fin.read()
+
+data = data.split()
+data = [int(i) for i in data]
+data = [i for i in data if i > 0]
+
+avg = sum(data) / len(data)
+
+data = [str(i) for i in data]
+
+data = ' '.join(data)
+
+file = argv[1] + " " + argv[2] + ".txt"
+
+with open(file=file, mode='w', encoding='utf8') as fout:
+  fout.write(f"ქულები:\n{data}\n\n")
+  fout.write(f"საშუალო: {avg:.2f}")
+
+
+input("Success!..")
