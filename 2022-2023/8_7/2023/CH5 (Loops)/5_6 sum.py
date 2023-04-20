1c3209c6cc4df4ed0d42625729e6f9ca7eff236b
pm72 <46081859+pm72@users.noreply.github.com>
2023-04-20 12:08:08 +0400

    pm72

---
+++ 2022-2023/8_7/2023/CH5 (Loops)/5_6 sum.py
@@ -0,0 +1,20 @@
+a, b, h = 2.14, 3.58, 0.014
+s = 0
+
+while a <= b:
+  s += a
+  a += h
+
+print(f"\n\n{s}")
+
+
+
+'''
+for i in range(N):   for i in range(10)
+  ...
+  ...
+  ...
+
+  range(a, b-1, h)
+
+'''
