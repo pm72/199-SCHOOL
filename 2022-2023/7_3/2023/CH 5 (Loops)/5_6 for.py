2a482764403e1b39e00094a44c95e64b53f77f2f
pm72 <46081859+pm72@users.noreply.github.com>
2023-04-21 12:52:37 +0400

    pm72

--- 2022-2023/7_3/2023/CH 5 (Loops)/5_6 for.py
+++ 2022-2023/7_3/2023/CH 5 (Loops)/5_6 for.py
@@ -20,12 +20,32 @@ a, b, h   [0.15, 0.36, 0.015]
 
 a, b, h = 0.15, 0.36, 0.015
 s = 0
-while a < b:
-  s += a
-  a += 0.015
+arr = []
 
-print(s)
+while a <= b:
+  arr.append(a)
+  s = round(s + a, 3)
+  a = round(a + h, 3)
+
+print(arr)
+print(f"The sum is {s}")
+
+print()
+
 
 '''
 N = [b - a / h] + 1  int((b - a) / h) + 1
 '''
+
+a, b, h = 0.15, 0.36, 0.015
+s = 0
+N = int((b - a) / h) + 1
+arr = []
+
+for i in range(N):
+  arr.append(a)
+  s = round(s + a, 3)
+  a = round(a + h, 3)
+
+print(arr)
+print(f"The sum is {s}")
