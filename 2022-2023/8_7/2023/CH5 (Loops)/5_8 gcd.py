ff57e88746d3398fa48f812c4f94dce0af6fcf0c
pm72 <46081859+pm72@users.noreply.github.com>
2023-05-04 11:55:19 +0400

    pm72

---
+++ 2022-2023/8_7/2023/CH5 (Loops)/5_8 gcd.py
@@ -0,0 +1,15 @@
+n1 = int(eval(input("Enter n1: ")))
+n2 = int(eval(input("Enter n2: ")))
+
+##n1, n2 = int(n1), int(n2)
+
+k = 2
+gcd = 1
+
+while k <= n1 and k <= n2:
+  if n1 % k == 0 and n2 % k == 0:
+    gcd = k
+
+  k += 1
+
+print(gcd)
