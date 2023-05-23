d4b925707e468d0d201e7dede764055ea40be4d7
pm72 <46081859+pm72@users.noreply.github.com>
2023-05-23 10:05:42 +0400

    pm72

---
+++ 2022-2023/7_1/2023/CH 5 (loops)/5_13__factorize_numbers.py
@@ -0,0 +1,14 @@
+number = int(eval(input("Enter a number: ")))
+divisor = 2
+
+while number > 1:
+  if number % divisor == 0:
+    print(f"{divisor:<5d}", end='')
+
+    number //= divisor
+  else:
+    divisor += 1
+
+
+# ============
+input("\n\nDone\n")
